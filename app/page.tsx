c7ec0261527f50b0b46122f08302f0552184b4fc
geekydev125 <goldenstardev1108@gmail.com>
2024-04-07 14:47:58 +0300

    Creating the VideoOverlay component and adding it with the video on the root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import VideoOverlay from "@/components/HomeViewComponents/VideoOverlay";
 import styles from "./page.module.css";
 import HomeViewBackgroundVideo from "@/components/HomeViewComponents/HomeViewBackgroundVideo";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<HomeViewBackgroundVideo />
+			<div className="overflow-hidden" style={{height: '100vh'}}>
+				<VideoOverlay />
+				<HomeViewBackgroundVideo />
+			</div>
 		</main>
 	);
 }
--- components/HomeViewComponents/HomeViewBackgroundVideo.tsx
+++ components/HomeViewComponents/HomeViewBackgroundVideo.tsx
@@ -12,9 +12,10 @@ function HomeViewBackgroundVideo() {
 
     return (
         <>
-            <button onClick={handlePlay}>Play Video</button>
+            <button className='d-none' onClick={handlePlay}>Play Video</button>
 
             <video
+                className='w-100 h-100 object-fit-cover'
                 poster="/assets/video/ny-timelapse/ny-timelapse-poster.jpg"
                 ref={videoRef}
                 autoPlay
@@ -25,6 +26,7 @@ function HomeViewBackgroundVideo() {
                 <source src="/assets/video/ny-timelapse/ny-timelapse.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+            
         </>
     )
 }
