73db822592ad948cf1ec2078c0bf16c442409f0b
geekydev125 <goldenstardev1108@gmail.com>
2024-04-08 15:55:46 +0300

    Creating the second div with the CTA button and adding a gradient bg to it

--- app/steamers/page.tsx
+++ app/steamers/page.tsx
@@ -1,13 +1,32 @@
+import styles from './page.module.scss'
+
 import BackgroundVideo from "@/components/BackgroundVideo"
+import IconDoubleDown from "@/components/Icons/IconDoubleDown"
+import VideoOverlay from '@/components/VideoOverlay'
 
 function SteamersPage() {
 	return (
-		<div>
+		<>
+			<VideoOverlay alpha={0.4} />
 			<BackgroundVideo
 				videoSrc="/assets/video/besteam/besteam.mp4"
 				posterSrc="/assets/video/besteam/besteam-poster.mp4"
+				classes="vh-100 w-100 position-absolute top-0 z-0 object-fit-cover"
 			/>
-		</div>
+
+			<div className="position-absolute top-50 z-1 w-100">
+				<div className='text-center w-100 mb-5'>
+					<h3 className='text-white text-uppercase'>The Revolutionary Travel Size Steamer From Italy</h3>
+					<IconDoubleDown fill='white' classes={styles['animated-icon']} width='30px' height='30px'/>
+				</div>
+
+				<div className={`text-center py-4 ${styles['gray-gradient-background']}`}>
+					<h3 className='text-white'>The steamer that has taken America by storm now available for purchase online</h3>
+					<button className='btn btn-success btn-lg mt-3'>GET YOURS NOW!</button>
+				</div>
+
+			</div>
+		</>
 	)
 }
 
