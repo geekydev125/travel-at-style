0108154d7e8593faf97801659be0c1d645726e77
geekydev125 <goldenstardev1108@gmail.com>
2024-04-08 15:55:14 +0300

    Adding an alpha prop to the VideoOverlay

--- components/VideoOverlay.tsx
+++ components/VideoOverlay.tsx
@@ -1,7 +1,13 @@
-function VideoOverlay() {
-  return (
-    <div className="position-absolute top-0 left-0 w-100 h-100 z-1" style={{backgroundColor: "rgba(0,0,0,0.5)"}} />
-  )
+interface Props {
+	alpha: number
+}
+
+function VideoOverlay({
+	alpha = 0.5
+}) {
+	return (
+		<div className="position-absolute top-0 left-0 w-100 h-100 z-1" style={{ backgroundColor: `rgba(0,0,0,${alpha})` }} />
+	)
 }
 
 export default VideoOverlay
