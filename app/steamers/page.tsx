f652886c86853689c3a5ad851c33fce51fe23c0b
geekydev125 <goldenstardev1108@gmail.com>
2024-04-08 14:10:44 +0300

    Implementing the Besteam video into the steamers page

--- app/steamers/page.tsx
+++ app/steamers/page.tsx
@@ -1,7 +1,14 @@
+import BackgroundVideo from "@/components/BackgroundVideo"
+
 function SteamersPage() {
-  return (
-    <div>SteamersPage</div>
-  )
+	return (
+		<div>
+			<BackgroundVideo
+				videoSrc="/assets/video/besteam/besteam.mp4"
+				posterSrc="/assets/video/besteam/besteam-poster.mp4"
+			/>
+		</div>
+	)
 }
 
 export default SteamersPage
