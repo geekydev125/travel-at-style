566fa2d1e69d889027620747a876cc191f89e7bf
geekydev125 <goldenstardev1108@gmail.com>
2024-04-08 17:28:26 +0300

    Applying proper padding and text alignment

--- components/SteamersViewComponents/SecondSectionBesteamDescription.tsx
+++ components/SteamersViewComponents/SecondSectionBesteamDescription.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import IconCheckCircle from "../Icons/IconCheckCircle"
 import AnimatedSteamerImg from "./AnimatedSteamerImg"
 
@@ -8,23 +7,22 @@ const steamerBulletPoints = [
 	"Produces powerful steam and can be used on all fabrics",
 	"Designed for world wide use; 110v and 220v compatible",
 	"Allowed to be taken as a carry-on on an airplane"
-
 ]
 
 function SecondSectionBesteamDescription() {
 	return (
 		<section className="container-fluid pt-5 pb-4 background-pattern-crossword">
 			<div className="row">
-				<div className="col-lg-4 col-sm-12 mx-auto px-4 p-md-4 pl-md-5">
+				<div className="col-sm-12 col-lg-4 mx-auto px-4 p-md-4 ps-md-5">
 					<h3 className="text-center mt-4 text-black">BeSteam</h3>
-					<p className="text-justify mt-4 text-black">BeSteam is the ultimate user-friendly light steam iron. Compact and lightweight, it is manufactured with high technology standards and made from high quality components &amp; materials. </p>
+					<p className="justify mt-4 text-black" style={{textAlign: 'justify'}}>BeSteam is the ultimate user-friendly light steam iron. Compact and lightweight, it is manufactured with high technology standards and made from high quality components &amp; materials.</p>
 				</div>
 
-				<div className="col-lg-4 col-sm-6 mt-sm-5 mt-md-5 mt-lg-5 mt-xl-0">
+				<div className="col-sm-6 col-lg-4 mt-sm-5 mt-md-5 mt-lg-5 mt-xl-0">
 					<AnimatedSteamerImg />
 				</div>
 
-				<div className="col-lg-4 col-sm-6 mx-auto px-4 pt-4 pt-md-5 pr-md-5">
+				<div className="col-sm-6 col-lg-4 mx-auto px-4 pt-4 pt-md-5 pe-md-5">
 					<ul className="list-unstyled">
 						{
 							steamerBulletPoints.map((bulletPoint, index) => {
