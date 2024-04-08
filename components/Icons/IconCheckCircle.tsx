80d3acbc109d3e708fdd13ec7a02052a7130a134
geekydev125 <goldenstardev1108@gmail.com>
2024-04-08 16:42:25 +0300

    Fixing the stroke/fill bug that wasn't coloring properly the icon

--- components/Icons/IconCheckCircle.tsx
+++ components/Icons/IconCheckCircle.tsx
@@ -1,7 +1,7 @@
 interface Props {
     width?: string,
     height?: string,
-    fill?: string,
+    stroke?: string,
     classes?: string
 }
 
@@ -9,14 +9,13 @@ interface Props {
 function IconCheckCircle({
     width = '20px',
     height = '20px',
-    fill = 'currentColor',
+    stroke = 'currentColor',
     classes
 }: Props) {
     return (
-        <svg className={classes} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={fill} style={{ width: `${width}`, height: `${height}` }}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        <svg className={classes} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={stroke} style={{ width: `${width}`, height: `${height}` }}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
-
     )
 }
 
