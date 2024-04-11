9b7247514f17a87126231f03ee1e33a72fcafdf9
geekydev125 <goldenstardev1108@gmail.com>
2024-04-11 21:02:35 +0300

    Fixing a bug that was not displaying the "User Instructions" nav item

--- components/Header/Nav/NavItems.tsx
+++ components/Header/Nav/NavItems.tsx
@@ -37,7 +37,7 @@ function NavItems() {
 
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 nav-underline">
                 {routes.map((route: Route, index) => {
-                    if (route.path !== '/' && index > 5) {
+                    if (route.path !== '/' && index > 4) {
                         return (
                             <li className="nav-item" key={uniqid()}>
                                 <Link className={`nav-link text-custom-light ${pathname === route.path && 'active'}`} aria-current="page" href={route.path}>{route.title}</Link>
