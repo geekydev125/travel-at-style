a05cd4dc6f5011b76158089931713cf3725e9504
geekydev125 <goldenstardev1108@gmail.com>
2024-04-11 16:03:33 +0300

    Creating the Steamer model

--- model/Steamer.ts
+++ model/Steamer.ts
@@ -1,9 +1,13 @@
 export class Steamer {
     constructor(
-        public name: number,
+        public name: string,
+        public imgFolder: string,
         public description: string,
         public packageIncludes: string[],
-        public variants: {}[],
+        public variants: {
+            color: string,
+            img: string
+        }[],
         public price: number
     ) { }
 }
