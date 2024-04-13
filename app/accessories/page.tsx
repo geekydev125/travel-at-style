77319b9ec433859f12a31becee83ee2bdbca0d31
geekydev125 <goldenstardev1108@gmail.com>
2024-04-13 07:36:48 +0300

    Implementing the title and img animation on the accessories page

--- app/accessories/page.tsx
+++ app/accessories/page.tsx
@@ -1,8 +1,33 @@
+"use client"
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 
 function AccessoriesPage() {
-  return (
-    <div>AccessoriesPage</div>
-  )
+	const imgRef = useRef(null);
+	const isInView = useInView(imgRef, { once: true })
+
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-md-5 d-flex flex-column justify-content-center align-items-center">
+					<img
+						src="/assets/img/airplane.png"
+						alt="Airplane"
+						ref={imgRef}
+						style={{
+							position: 'relative',
+							transition: 'all 1.1s ease-in-out',
+							opacity: isInView ? 1 : 0,
+							left: isInView ? 0 : -200
+						}}
+					/>
+				</div>
+				<div className="col-md-7 d-flex flex-column justify-content-center">
+					<h2 className="display-4 text-center text-custom-dark">TRAVEL ACCESSORIES</h2>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default AccessoriesPage
