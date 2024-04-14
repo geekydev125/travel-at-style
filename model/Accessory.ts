f52f62c7a7528aff29d22dad8e7025d97c74c54e
geekydev125 <goldenstardev1108@gmail.com>
2024-04-14 08:16:56 +0300

    Creating the Accessory data model

---
+++ model/Accessory.ts
@@ -0,0 +1,9 @@
+export class Accessory {
+    constructor(
+        public name: string,
+        public imgFolder: string,
+        public img: string,
+        public description: string,
+        public price: number
+    ) { }
+}
