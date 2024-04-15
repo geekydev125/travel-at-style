e15fe394ea655635aae50838c49ec14d77b8f9c6
geekydev125 <goldenstardev1108@gmail.com>
2024-04-15 18:30:23 +0300

    Creating a SteamerVariant interface and updating the Steamer class

--- model/Steamer.ts
+++ model/Steamer.ts
@@ -1,13 +1,16 @@
+export interface SteamerVariant {
+    color: string,
+    img: string,
+    imgShop: string
+}
+
 export class Steamer {
     constructor(
         public name: string,
         public imgFolder: string,
         public description: string,
         public packageIncludes: string[],
-        public variants: {
-            color: string,
-            img: string
-        }[],
+        public variants: SteamerVariant[],
         public price: number
     ) { }
 }
