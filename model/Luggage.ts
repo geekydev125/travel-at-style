2ab1adc08e1ff709cdacf639f93035dc453eb02d
geekydev125 <goldenstardev1108@gmail.com>
2024-04-15 09:41:00 +0300

    Creating the Luggage model

---
+++ model/Luggage.ts
@@ -0,0 +1,9 @@
+export class Luggage {
+    constructor(
+        public name: string,
+        public imgFolder: string,
+        public img: string,
+        public description: string,
+        public price: number
+    ) { }
+}
