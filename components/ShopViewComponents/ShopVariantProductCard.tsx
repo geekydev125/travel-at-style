e3416c0ffbecab99c6f9981e862bf7dead4ceb5b
geekydev125 <goldenstardev1108@gmail.com>
2024-04-15 20:02:46 +0300

    Adding the onClick functionality to open the product modal

--- components/ShopViewComponents/ShopVariantProductCard.tsx
+++ components/ShopViewComponents/ShopVariantProductCard.tsx
@@ -2,14 +2,18 @@ import { Steamer, SteamerVariant } from '@/model/Steamer'
 
 import styles from './ShopProductCard.module.scss'
 
+import Button from 'react-bootstrap/Button'
+
 interface Props {
 	product: Steamer,
-	variant: SteamerVariant
+	variant: SteamerVariant,
+    showModalHandler: (showBool: boolean, product?: Steamer) => void
 }
 
 function ShopVariantProductCard({
 	product,
-	variant
+	variant,
+	showModalHandler
 }: Props) {
 	return (
 		<div className="position-relative background-light-gradient" style={{ backgroundColor: 'lightgray' }}>
@@ -18,13 +22,13 @@ function ShopVariantProductCard({
 			</span>
 
 			<div className="overflow-hidden position-relative" >
-				<img className={`img-fluid ${styles['shop-product-card-img']} `} src={`/assets/img/${product.imgFolder}/${variant.imgShop}`} alt={product.name} />
+				<img onClick={() => showModalHandler(true, product)} className={`img-fluid ${styles['shop-product-card-img']} `} src={`/assets/img/${product.imgFolder}/${variant.imgShop}`} alt={product.name} />
 			</div>
 
 			<div className="d-flex justify-content-between align-items-center px-2 py-4 position-relative overflow-hidden">
 
-				<span className="text-custom-dark fw-bold h5 mb-0">{product.name} ({variant.color})</span>
-				<button className="btn btn-success btn-sm me-2">Add to cart</button>
+				<Button variant='text' onClick={() => showModalHandler(true, product)} className="text-custom-dark fw-bold h5 mb-0">{product.name} ({variant.color})</Button>
+				<Button variant='button' className="btn btn-success btn-sm me-2">Add to cart</Button>
 			</div>
 
 		</div>
