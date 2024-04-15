e9a18d69c3d88dfc0e54039534d1b20fe1b4fb00
geekydev125 <goldenstardev1108@gmail.com>
2024-04-15 14:29:46 +0300

    Populating the luggage page

--- app/(products)/(with-layout)/luggage/page.tsx
+++ app/(products)/(with-layout)/luggage/page.tsx
@@ -1,7 +1,26 @@
+"use client"
+import uniqid from 'uniqid'
+
+import ProductCard from '@/components/ProductCard'
+
+import luggage from '@/data/luggage.json'
+
+import { Luggage } from '@/model/Luggage'
+
 function LuggagePage() {
-  return (
-    <div>LuggagePage</div>
-  )
+	return (
+		<section className="row gy-4">
+			{
+				Object.values(luggage).map((accessory: Luggage) => {
+					return (
+						<div className="col-12 col-md-4" key={uniqid()}>
+							<ProductCard product={accessory} />
+						</div>
+					)
+				})
+			}
+		</section>
+	)
 }
 
 export default LuggagePage
