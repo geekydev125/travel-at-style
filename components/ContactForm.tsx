266235cacf8aea73f79c328d829ff774b02678cb
geekydev125 <goldenstardev1108@gmail.com>
2024-04-17 19:46:36 +0300

    Changing the ternary operators

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -38,24 +38,24 @@ function ContactForm() {
         <Form onSubmit={handleSubmit(onFormSubmit)} data-bs-theme="dark" className='w-100'>
             
             <FloatingLabel className='' label="Name" controlId="name" >
-                <Form.Control isInvalid={errors.name ? true : false} {...register('name')} type="text" placeholder="Enter your name" name='name' />
+                <Form.Control isInvalid={errors.name && true} {...register('name')} type="text" placeholder="Enter your name" name='name' />
                 
             </FloatingLabel>
             <p className="text-danger mb-2">{errors.name && errors.name.message}</p>
 
             <FloatingLabel className='' label="Email Address" controlId="email" >
-                <Form.Control isInvalid={errors.email ? true : false} {...register('email')} type="email" placeholder="Enter email" name='email' />
+                <Form.Control isInvalid={errors.email && true} {...register('email')} type="email" placeholder="Enter email" name='email' />
             </FloatingLabel>
             <p className="text-danger mb-2">{errors.email && errors.email.message}</p>
 
 
             <FloatingLabel className='' label="Subject" controlId="subject" >
-                <Form.Control isInvalid={errors.subject ? true : false} {...register('subject')} type="text" placeholder="Enter subject" name='subject' />
+                <Form.Control isInvalid={errors.subject && true} {...register('subject')} type="text" placeholder="Enter subject" name='subject' />
             </FloatingLabel>
             <p className="text-danger mb-2">{errors.subject && errors.subject.message}</p>
 
             <FloatingLabel className='' label="Message" controlId="message" >
-                <Form.Control isInvalid={errors.message ? true : false} {...register('message')} as='textarea' style={{ height: '100px' }} placeholder="Enter your message" name='message' />
+                <Form.Control isInvalid={errors.message && true} {...register('message')} as='textarea' style={{ height: '100px' }} placeholder="Enter your message" name='message' />
             </FloatingLabel>
             <p className="text-danger mb-2">{errors.message && errors.message.message}</p>
 
