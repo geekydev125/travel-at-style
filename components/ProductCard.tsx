14ccc9007c4f80942c2cb886f94db42e12967dd7
geekydev125 <goldenstardev1108@gmail.com>
2024-04-18 21:05:45 +0300

    Restructuring and styling the ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link"
+import NextLink from "next/link"
 
 import { Accessory } from "@/model/Accessory"
 import { Luggage } from "@/model/Luggage"
 import styles from "./ProductCard.module.scss"
+import GreenButton from "@/components/GreenButton"
 
 import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button"
 
 interface Props {
     product: Accessory | Luggage
@@ -15,7 +15,7 @@ function ProductCard({
     product,
 }: Props) {
     return (
-        <Card className={`shadow-sm ${styles['accessory-card']}`}>
+        <Card className={`shadow-sm ${styles['accessory-card']} background-radial-gradient `}>
             <div className="m-2 rounded-1 overflow-hidden">
                 <Card.Img
                     variant="top"
@@ -24,19 +24,26 @@ function ProductCard({
                     alt={product.name}
                 />
             </div>
-            <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text className="text-custom-dark">
-                    {product.description}
-                </Card.Text>
-                <Card.Text className="text-custom-dark h4">
-                    ${product.price}
-                </Card.Text>
 
-                <div className="d-flex justify-content-center">
-                    <Link href="/shop" className="text-center">
-                        <Button variant="success" className="">GET YOURS NOW!</Button>
-                    </Link>
+            <Card.Body className="p-0 pt-3 d-flex flex-column justify-content-between" style={{borderTop: '1px solid #f5f5f5'}}>
+                <Card.Title className="px-2 display-5 fw-semibold">{product.name}</Card.Title>
+                <div>
+                    <Card.Text className="text-custom-dark px-2">
+                        {product.description}
+                    </Card.Text>
+
+                    <div className="background-light-gradient p-3 d-flex flex-row flex-sm-column flex-lg-row justify-content-between align-items-center ">
+                        <Card.Text className="text-custom-dark display-5 fw-semibold mb-0">
+                            ${product.price}
+                        </Card.Text>
+                        <NextLink href="/shop" >
+                            <GreenButton >GET YOURS NOW!</GreenButton>
+                        </NextLink>
+
+                        {/* <div className="d-flex justify-content-center">
+
+                        </div> */}
+                    </div>
                 </div>
             </Card.Body>
         </Card>
