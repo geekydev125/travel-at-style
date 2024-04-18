b3eaa7172edd40fc59ad95ed3b7def2a03a6fadc
geekydev125 <goldenstardev1108@gmail.com>
2024-04-18 19:19:26 +0300

    Fixing the overflowing iframe and optimizing for mobile

--- components/SteamersViewComponents/SectionComponents/ThirdSectionBesteamVideo.tsx
+++ components/SteamersViewComponents/SectionComponents/ThirdSectionBesteamVideo.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Parallax } from "react-parallax"
 
+import styles from './ThirdSectionBesteamVideo.module.scss'
+
 function ThirdSectionBesteamVideo() {
     return (
-        <Parallax blur={3} bgImage="/assets/video/ny-timelapse/ny-timelapse-poster.jpg" bgImageAlt="New York at night" strength={100} >
-            <div className="text-center rounded " style={{padding: '80px', backgroundColor: 'rgba(0,0,0,0.5)'}}>
-                <h3 className="mb-5 text-custom-light">Check out Besteam in action!</h3>
-                <iframe className='rounded-4' width="730" height="411" src="https://www.youtube.com/embed/_z4EY7BYz6M?si=VBu5hRD1S49iXrnf&amp;controls=0&showinfo=0" 
+        <Parallax blur={3} bgImage="/assets/img/ny-skyline.jpg" bgImageAlt="New York at night" strength={100} >
+            <div className={`${styles['container-div']} text-center rounded py-5 py-lg-5 px-3`}>
+                <h3 className="mb-3 mb-lg-5 text-custom-light text-capitalize">Check out Besteam in action!</h3>
+                <iframe className={`${styles.iframe} rounded-4`} src="https://www.youtube.com/embed/_z4EY7BYz6M?si=VBu5hRD1S49iXrnf&amp;controls=0&showinfo=0" 
                 title="Besteam Video" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                 allowFullScreen></iframe>
