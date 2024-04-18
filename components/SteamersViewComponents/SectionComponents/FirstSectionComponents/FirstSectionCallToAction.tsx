e6eff5bb134994733a06d6286a3f57303f79727a
geekydev125 <goldenstardev1108@gmail.com>
2024-04-18 18:28:47 +0300

    Optimizing for mobile and implementing the GreenButton component

--- components/SteamersViewComponents/SectionComponents/FirstSectionComponents/FirstSectionCallToAction.tsx
+++ components/SteamersViewComponents/SectionComponents/FirstSectionComponents/FirstSectionCallToAction.tsx
@@ -1,10 +1,18 @@
+import NextLink from 'next/link'
+
 import styles from './FirstSectionCallToAction.module.scss'
+import GreenButton from '@/components/GreenButton'
 
 function FirstSectionCallToAction() {
     return (
-        <section className={`${styles.section} text-center py-4 background-gray-gradient`} >
-            <h3>The steamer that has taken America by storm now available for purchase online</h3>
-            <button className='btn btn-success btn-lg mt-3'>GET YOURS NOW!</button>
+        <section className={`${styles.section} text-center px-3 py-3 py-lg-4 background-gray-gradient`} >
+            <h3 className="display-3 text-capitalize">The steamer that has taken America by storm now available for purchase online</h3>
+
+            <NextLink href='/shop'>
+                <GreenButton classes='mt-1 mt-md-3'>
+                    GET YOURS NOW!
+                </GreenButton>
+            </NextLink>
         </section>
     )
 }
