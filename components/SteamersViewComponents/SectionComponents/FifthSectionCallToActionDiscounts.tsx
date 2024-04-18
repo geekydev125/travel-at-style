4081e65fdb999869fcb2de055f964aceca26c45f
geekydev125 <goldenstardev1108@gmail.com>
2024-04-18 20:28:25 +0300

    Rearranging columns on mobile, adjusting paddings and styles

--- components/SteamersViewComponents/SectionComponents/FifthSectionCallToActionDiscounts.tsx
+++ components/SteamersViewComponents/SectionComponents/FifthSectionCallToActionDiscounts.tsx
@@ -1,24 +1,28 @@
+import GreenButton from "@/components/GreenButton"
 import Link from "next/link"
 
 function FifthSectionCallToActionDiscounts() {
     return (
-        <section className="background-pattern-wool py-5">
+        <section className="background-pattern-wool px-2 py-5">
             <div className="container background-gray-gradient px-3 py-3">
-                <div className="row">
-                    <div className="col-2 d-flex justify-content-center align-items-center">
-                        <img src="/assets/img/lifetime-warranty.png" alt="Lifetime Warranty" className="" />
+                <div className="row gx-md-0">
+
+                    <div className="col-4 col-md-3 col-lg-2 order-2 order-md-1 order-lg-1 d-flex justify-content-center align-items-center">
+                        <img src="/assets/img/lifetime-warranty.png" alt="Lifetime Warranty" className="img-fluid" />
                     </div>
-                    <div className="col-md-7 text-center d-flex flex-column justify-content-center align-items-center">
-                        <h3 className="text-custom-dark">OVER THE TOP QUANTITY BASED DISCOUNTS</h3>
+
+                    <div className="col-12 col-md-6 col-lg-7 order-1 order-md-2 order-lg-2 text-center d-flex flex-column justify-content-center align-items-center">
+                        <h3 className="text-capitalize">Over the top quantity-based discounts</h3>
 
                         <Link href="/shop">
-                            <button className="btn btn-success btn-lg shadow mt-2 mb-3">GET YOURS NOW!</button>
+                            <GreenButton classes="mt-md-2 mb-3">GET YOURS NOW!</GreenButton>
                         </Link>
 
-                        <h3 className="text-custom-dark">LIFETIME WARRANTY</h3>
+                        <h3 className="text-capitalize">Lifetime Warranty</h3>
                     </div>
-                    <div className="col-3 d-flex justify-content-center align-items-center ">
-                        <img src="/assets/img/multiple-steamers.png" className="mx-auto" alt="Multiple Steamers" />
+
+                    <div className="col-8 col-md-3 col-lg-3 order-3 d-flex justify-content-center align-items-center ">
+                        <img src="/assets/img/multiple-steamers.png" className="img-fluid" alt="Multiple Steamers" />
                     </div>
                 </div>
             </div>
