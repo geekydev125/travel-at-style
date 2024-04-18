6a01d9e092c48df5ed76724e85efbfafa7b42d54
geekydev125 <goldenstardev1108@gmail.com>
2024-04-18 22:15:31 +0300

    Restructuring and styling the product modal

--- components/ProductModal.tsx
+++ components/ProductModal.tsx
@@ -5,30 +5,46 @@ import { Steamer } from '@/model/Steamer';
 
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import GreenButton from '@/components/GreenButton';
+import IconPlus from './Icons/IconPlus';
 
 function ProductModal() {
     const { showModal, product, showModalHandler } = useModalContext()
 
     return (
         <>
-            <Modal size='xl' centered show={showModal} onHide={() => showModalHandler(false, null)}>
-                <Modal.Header closeButton={false} className='background-light-gradient text-custom-dark'>
-                    <Modal.Title>{product && product.name} {`(${(product as Steamer).color})`} </Modal.Title>
-                </Modal.Header>
+            <Modal size='lg' centered show={showModal} onHide={() => showModalHandler(false, null)}>
+                {/* <Modal.Header closeButton={false} className='background-light-gradient text-custom-dark'>
+                    <Modal.Title>
+                        {product && product.name}
+                        {`${(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}`}
+                    </Modal.Title>
+                </Modal.Header> */}
 
-                <Modal.Body className='background-pattern-crossword text-custom-dark'>
+                <Modal.Body className='background-pattern-crossword text-custom-dark rounded-3'>
                     <div className='container'>
                         <div className='row'>
-                            <div className='col-12 col-lg-3 mb-3 mb-lg-0'>
-                                <p className='text-custom-dark'>
-                                    {product && product.description}
-                                </p>
-                                <p className='text-custom-dark h4'>
-                                   Price: {product && product.price}
-                                </p>
-                                <Button variant='button' className="btn btn-success me-2">Add to cart</Button>
+                            <div className='col-12 col-lg-5 mb-3 mb-lg-0 d-flex flex-column justify-content-between '>
+                                <div>
+                                    <h4 className='text-custom-dark display-4 fw-semibold'>
+                                        {product && product.name}
+                                        {`${(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}`}
+                                    </h4>
+
+                                    <p className='text-custom-dark mt-4'>
+                                        {product && product.description}
+                                    </p>
+                                </div>
+
+                                <div className='d-none d-lg-flex flex-row justify-content-between align-items-center'>
+                                    <p className='text-custom-dark display-5 fw-semibold mb-0'>
+                                        Price: {product && product.price}
+                                    </p>
+                                    <GreenButton size='sm'>Add to cart <IconPlus /></GreenButton>
+                                </div>
+                               
                             </div>
-                            <div className='col-12 col-lg-9 overflow-hidden d-flex flex-row align-items-center justify-content-end'>
+                            <div className='col-12 col-lg-7 overflow-hidden '>
                                 {
                                     product && (
                                         <img
@@ -38,6 +54,13 @@ function ProductModal() {
                                         />
                                     )
                                 }
+
+                                <div className='d-flex d-lg-none flex-column flex-sm-row justify-content-between align-items-center mt-4'>
+                                    <p className='text-custom-dark display-5 fw-semibold mb-0'>
+                                        Price: {product && product.price}
+                                    </p>
+                                    <GreenButton size='sm' classes='mt-2 mt-sm-0'>Add to cart <IconPlus /></GreenButton>
+                                </div>
                             </div>
                         </div>
                     </div>
