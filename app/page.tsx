1f7fee6f0af976206814c537b9c44833f4a8c45b
geekydev125 <goldenstardev1108@gmail.com>
2024-04-18 15:29:46 +0300

    Redirecting from the "/" path to "/steamers"

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import VideoOverlay from "@/components/VideoOverlay";
-import BackgroundVideo from "@/components/BackgroundVideo";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-	return (
-		<div className="overflow-hidden z-1" style={{ height: '100vh' }}>
-			<VideoOverlay />
-
-			<BackgroundVideo
-				videoSrc="/assets/video/ny-timelapse/ny-timelapse.mp4"
-				posterSrc="/assets/video/ny-timelapse/ny-timelapse-poster.jpg"
-			/>
-		</div>
-	);
+	redirect('/steamers');
+	
+	return null;
 }
