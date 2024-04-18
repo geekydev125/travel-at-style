d3e2df62a5ff59c08696bbe4390492f5e172f77e
geekydev125 <goldenstardev1108@gmail.com>
2024-04-18 15:30:02 +0300

    Removing the conditional rendering of the header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-
 import Nav from "@/components/Header/Nav/Nav";
 
 function Header() {
-    const pathname = usePathname();
 
     return (
-        <>
-            {
-                pathname !== "/" && (
-                    <header className='position-sticky top-0 z-2 px-md-5 shadow background-pattern-wood'>
-                        <Nav />
-                    </header>
-                )
-            }
-        </>
+        <header className='position-sticky top-0 z-2 px-md-5 shadow background-pattern-wood'>
+            <Nav />
+        </header>
     )
 }
 
