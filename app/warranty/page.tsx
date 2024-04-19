bf39311dc1d83a5fccdf806481fdb1be36d94aa7
geekydev125 <goldenstardev1108@gmail.com>
2024-04-19 16:20:06 +0300

    Populating the warranty page with content

--- app/warranty/page.tsx
+++ app/warranty/page.tsx
@@ -1,7 +1,27 @@
 function WarrantyPage() {
-  return (
-    <div>WarrantyPage</div>
-  )
+	return (
+		<section className="container py-5">
+			<div className="row">
+				<div className="col-sm-9">
+					<h3 className="text-custom-dark">Warranty</h3>
+
+					<div className="ps-md-4">
+						<p className="text-custom-dark">If you experience a manufacturing defect in materials, <span className="fst-italic text-custom-dark">Travel @ Style</span> warrants your steamer  and all of your accessories for the lifetime of the product. We will repair or replace the product at no charge.</p>
+
+						<p className="text-custom-dark">To receive your warranty&nbsp;
+							<a href="http://www.besteamitaly.com/warranty" target="_blank">CLICK HERE</a>
+						</p>
+					</div>
+
+				</div>
+				<div className="col-sm-3 d-flex flex-column justify-content-center justify-content-md-start align-items-center">
+					<a href="http://www.besteamitaly.com/warranty" target="_blank">
+						<img src="/assets/img/lifetime-warranty.png" className="img-fluid mt-md-5 mt-lg-0 mx-auto" alt="Lifetime Warranty Logo" />
+					</a>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default WarrantyPage
