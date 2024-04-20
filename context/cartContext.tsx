2dff8d8a3707a9df620c57a31a6675a7c9d2f0db
geekydev125 <goldenstardev1108@gmail.com>
2024-04-20 08:02:21 +0300

    Implementing the addProductToCart functionality in the cart context

--- context/cartContext.tsx
+++ context/cartContext.tsx
@@ -1,11 +1,15 @@
 "use client";
 import useLocalStorage from "@/hooks/useLocalStorage";
+import { Accessory } from "@/model/Accessory";
+import { Luggage } from "@/model/Luggage";
 import { Product } from "@/model/Product";
+import { Steamer } from "@/model/Steamer";
 import { createContext, useContext } from "react";
 
 export interface ICartProduct {
     _id: Product['_id'];
     productCategory: Product['productCategory'];
+    quantity: number;
 }
 
 export type ICart = ICartProduct[] | [];
@@ -14,12 +18,12 @@ const cartInitialState: ICart = []
 
 interface ICartContext {
     cart: ICart,
-    addProduct: (product: ICartProduct) => void;
+    addProductToCart: (product: Steamer | Luggage | Accessory) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
     cart: cartInitialState,
-    addProduct: () => {}
+    addProductToCart: () => {}
 });
 
 interface Props {
@@ -31,17 +35,22 @@ export const CartContextProvider = ({
 }: Props) => {
     const [cart, setCart] = useLocalStorage('cart', cartInitialState);
 
-    const addProduct = (product: ICartProduct) => {
-        let newProduct = {
+    const addProductToCart = (product: Steamer | Luggage | Accessory) => {
+        let quantityProductsInCart = cart.find((cartProduct:ICartProduct) => cartProduct._id === product._id)?.quantity || 0;
+
+        let newProduct: ICartProduct = {
             _id: product._id,
-            productCategory: product.productCategory
+            productCategory: product.productCategory,
+            quantity: quantityProductsInCart + 1
         }
 
-        setCart([...cart, newProduct]);
+        let filteredCartWithoutCurrentProduct = cart.filter((cartProduct:ICartProduct) => cartProduct._id !== product._id);
+
+        setCart([...filteredCartWithoutCurrentProduct, newProduct]);
     }
 
     return (
-        <CartContext.Provider value={{cart, addProduct}}>
+        <CartContext.Provider value={{cart, addProductToCart}}>
             {children}
         </CartContext.Provider>
     )
