42df87d480aa7e64beda12ced093289b606e767f
geekydev125 <goldenstardev1108@gmail.com>
2024-04-21 09:12:51 +0300

    Updating the fields that are passed to the cart object + fixing bugs in addProductToCart functionality

--- context/cartContext.tsx
+++ context/cartContext.tsx
@@ -7,8 +7,11 @@ import { Steamer } from "@/model/Steamer";
 import { createContext, useContext } from "react";
 
 export interface ICartProduct {
-    _id: Product['_id'];
-    productCategory: Product['productCategory'];
+    _id: Product['_id'],
+    productCategory: Product['productCategory'],
+    name: Product['name'],
+    img: Product['img'],
+    price: Product['price'],
     quantity: number;
 }
 
@@ -19,11 +22,13 @@ const cartInitialState: ICart = []
 interface ICartContext {
     cart: ICart,
     addProductToCart: (product: Steamer | Luggage | Accessory) => void;
+    getAllProductsInCart: () => ICart;
 }
 
 export const CartContext = createContext<ICartContext>({
     cart: cartInitialState,
-    addProductToCart: () => {}
+    addProductToCart: () => { },
+    getAllProductsInCart: () => cartInitialState
 });
 
 interface Props {
@@ -36,21 +41,42 @@ export const CartContextProvider = ({
     const [cart, setCart] = useLocalStorage('cart', cartInitialState);
 
     const addProductToCart = (product: Steamer | Luggage | Accessory) => {
-        let quantityProductsInCart = cart.find((cartProduct:ICartProduct) => cartProduct._id === product._id)?.quantity || 0;
+        // check if the product is already in the cart
+        let isProductAlreadyInCart = cart.find((cartProduct: ICartProduct) => cartProduct._id === product._id) || 0;
 
-        let newProduct: ICartProduct = {
-            _id: product._id,
-            productCategory: product.productCategory,
-            quantity: quantityProductsInCart + 1
+        if (isProductAlreadyInCart) {
+            let indexOfProductInCart = cart.indexOf(isProductAlreadyInCart);
+            let newInstanceOfCart = [...cart];
+
+            let updatedProduct: ICartProduct = {
+                ...isProductAlreadyInCart,
+                quantity: isProductAlreadyInCart.quantity + 1
+            }
+            
+            newInstanceOfCart[indexOfProductInCart] = updatedProduct;
+
+            return setCart(( previousCart:ICart ) => newInstanceOfCart);
+        } else {
+            // create the new product
+            let newProduct: ICartProduct = {
+                _id: product._id,
+                productCategory: product.productCategory,
+                name: product.name,
+                img: `/assets/img/${product.imgFolder}/${product.img}`,
+                price: product.price,
+                quantity: 1
+            }
+
+            setCart(( previousCart:ICart ) => [...previousCart, newProduct]);
         }
+    }
 
-        let filteredCartWithoutCurrentProduct = cart.filter((cartProduct:ICartProduct) => cartProduct._id !== product._id);
-
-        setCart([...filteredCartWithoutCurrentProduct, newProduct]);
+    const getAllProductsInCart = () => {
+        return cart;
     }
 
     return (
-        <CartContext.Provider value={{cart, addProductToCart}}>
+        <CartContext.Provider value={{ cart, addProductToCart, getAllProductsInCart }}>
             {children}
         </CartContext.Provider>
     )
