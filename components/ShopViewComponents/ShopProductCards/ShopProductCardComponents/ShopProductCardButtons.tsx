9873074c78ac150736cacb96480ab5af4e717dbd
geekydev125 <goldenstardev1108@gmail.com>
2024-04-22 09:05:09 +0300

    Conditionally displaying a remove from cart button if product is present in cart

--- components/ShopViewComponents/ShopProductCards/ShopProductCardComponents/ShopProductCardButtons.tsx
+++ components/ShopViewComponents/ShopProductCards/ShopProductCardComponents/ShopProductCardButtons.tsx
@@ -1,15 +1,16 @@
 
 import { useModalContext } from "@/context/modalContext"
+import { useCartContext } from "@/context/cartContext"
 
 import { Accessory } from "@/model/Accessory"
 import { Luggage } from "@/model/Luggage"
 import { Steamer } from "@/model/Steamer"
 
-import GreenButton from "@/components/GreenButton"
+import CustomButton from "@/components/CustomButton"
 import IconPlus from "@/components/Icons/IconPlus"
+import IconMinus from "@/components/Icons/IconMinus"
 
 import Button from "react-bootstrap/Button"
-import { useCartContext } from "@/context/cartContext"
 
 interface Props {
     product: Steamer | Luggage | Accessory,
@@ -19,15 +20,27 @@ function ShopProductCardButtons({
     product,
 }: Props) {
     const { showModalHandler } = useModalContext()
-    const { addProductToCart } = useCartContext()
+    const { addProductToCart, removeProductFromCart, isProductInCart } = useCartContext()
 
     return (
         <div className="d-flex flex-row flex-sm-column flex-xl-row justify-content-between align-items-center px-2 py-4 position-relative overflow-hidden">
             <Button variant='text' onClick={() => showModalHandler(true, product)} className="text-custom-dark fw-bold h5 mb-0 px-md-0 px-lg-2">
-                {product.name} 
+                {product.name}
                 {(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}
             </Button>
-            <GreenButton onClick={() => addProductToCart(product)} size='sm' classes="me-2 mt-sm-1 mt-xl-0">Add to cart <IconPlus /></GreenButton>
+            <CustomButton variant="success" onClick={() => addProductToCart(product)} size='sm' classes="me-2 mt-sm-1 mt-xl-0">
+                Add to cart&nbsp;
+                <IconPlus />
+            </CustomButton>
+            {
+                isProductInCart(product) && (
+                    <CustomButton variant="danger" size="sm" onClick={() => removeProductFromCart(product)}>
+                        Remove from cart&nbsp;
+                        <IconMinus />
+                    </CustomButton>
+                )
+            }
+
         </div>
     )
 }
