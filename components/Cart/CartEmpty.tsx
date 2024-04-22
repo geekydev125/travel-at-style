0b67f9c5001fa7ec1c9729292706e61ade323414
geekydev125 <goldenstardev1108@gmail.com>
2024-04-22 18:38:53 +0300

    Drawer closes on every click of the "shop" link

--- components/Cart/CartEmpty.tsx
+++ components/Cart/CartEmpty.tsx
@@ -1,5 +1,4 @@
 import NextLink from "next/link";
-import { usePathname } from "next/navigation";
 
 interface Props {
     handleDrawerClose: () => void
@@ -8,14 +7,12 @@ interface Props {
 function CartEmpty({
     handleDrawerClose
 }:Props) {
-    const pathname = usePathname()
-
     return (
         <div className="d-flex flex-column justify-content-center h-100">
             <h5 className="text-custom-dark">Cart is empty</h5>
             <p className="text-custom-dark">
                 Please head out to the&nbsp;
-                <NextLink href='/shop' onClick={ pathname === '/shop' ? handleDrawerClose : undefined } >shop</NextLink>
+                <NextLink href='/shop' onClick={ handleDrawerClose } >shop</NextLink>
                 &nbsp;to fill up the cart with your travel gear.</p>
         </div>
     )
