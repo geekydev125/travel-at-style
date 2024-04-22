5fb998129646c735c5a922e392823239a3d5e539
geekydev125 <goldenstardev1108@gmail.com>
2024-04-22 08:55:08 +0300

    Renaming the GreenButton component to CustomButton

---
+++ components/CustomButton.tsx
@@ -0,0 +1,25 @@
+import Button from "react-bootstrap/Button"
+
+interface Props {
+    size?: 'sm' | 'lg' | undefined
+    variant: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link' | undefined
+    classes?: string
+    children: React.ReactNode
+    onClick?: () => void
+}
+
+function CustomButton({
+    size,
+    variant,
+    classes,
+    onClick,
+    children,
+}: Props) {
+    return (
+        <Button onClick={onClick} size={size} variant={variant} className={`shadow ${classes}`} style={{border: '1px solid #32a670'}}>
+            {children}
+        </Button>
+    )
+}
+
+export default CustomButton
