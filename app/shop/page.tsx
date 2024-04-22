c894bf8305b8d057e1cde1bca6b7f78c6c75300e
geekydev125 <goldenstardev1108@gmail.com>
2024-04-22 09:05:55 +0300

    Adding the CartDrawer to the shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,12 +3,14 @@ import { useModalContext } from "@/context/modalContext"
 
 import ProductsTabs from "@/components/ShopViewComponents/ProductsTabs"
 import ProductModal from "@/components/ProductModal"
+import CartDrawer from "@/components/CartDrawer"
 
 function ShopPage() {
 	const { showModal } = useModalContext()
 
 	return (
 		<section className="container py-5">
+			<CartDrawer />
 			<ProductsTabs />
 
 			{showModal && <ProductModal />}		
