6f7ae76e6357baf626550061f6c90cd8e8a602a6
geekydev125 <goldenstardev1108@gmail.com>
2024-04-22 11:34:33 +0300

    Creating the increaseProductQuantity and modifying the isProductInCart  to take an _id

--- context/cartContext.tsx
+++ context/cartContext.tsx
@@ -16,21 +16,24 @@ export interface ICartProduct {
 }
 
 export type ICart = ICartProduct[] | [];
+export type TProduct = Steamer | Luggage | Accessory;
 
 const cartInitialState: ICart = []
 
 interface ICartContext {
     cart: ICart,
-    addProductToCart: (product: Steamer | Luggage | Accessory) => void,
-    removeProductFromCart: (product: Steamer | Luggage | Accessory) => void,
-    isProductInCart: (product: Steamer | Luggage | Accessory) => boolean
+    addProductToCart: (product: TProduct) => void,
+    removeProductFromCart: (product: TProduct) => void,
+    isProductInCart: (productId: TProduct['_id'] | ICartProduct['_id']) => boolean,
+    increaseProductQuantity: (product: ICartProduct) => void
 }
 
 export const CartContext = createContext<ICartContext>({
     cart: cartInitialState,
-    addProductToCart: () => {},
-    removeProductFromCart: () => {},
-    isProductInCart: () => false
+    addProductToCart: () => { },
+    removeProductFromCart: () => { },
+    isProductInCart: () => false,
+    increaseProductQuantity: () => { }
 });
 
 interface Props {
@@ -42,22 +45,24 @@ export const CartContextProvider = ({
 }: Props) => {
     const [cart, setCart] = useLocalStorage('cart', cartInitialState);
 
-    const addProductToCart = (product: Steamer | Luggage | Accessory) => {
+    const addProductToCart = (product: TProduct) => {
         // check if the product is already in the cart
-        let isProductAlreadyInCart = cart.find((cartProduct: ICartProduct) => cartProduct._id === product._id) || 0;
+        let isProductAlreadyInCart = cart.find((cartProduct: ICartProduct) => cartProduct._id === product._id);
 
         if (isProductAlreadyInCart) {
-            let indexOfProductInCart = cart.indexOf(isProductAlreadyInCart);
+            let productAlreadyInCart = isProductAlreadyInCart;
+
+            let indexOfProductInCart = cart.indexOf(productAlreadyInCart);
             let newInstanceOfCart = [...cart];
 
             let updatedProduct: ICartProduct = {
-                ...isProductAlreadyInCart,
-                quantity: isProductAlreadyInCart.quantity + 1
+                ...productAlreadyInCart,
+                quantity: productAlreadyInCart.quantity + 1
             }
-            
+
             newInstanceOfCart[indexOfProductInCart] = updatedProduct;
 
-            return setCart(( previousCart:ICart ) => newInstanceOfCart);
+            return setCart((previousCart: ICart) => newInstanceOfCart);
         } else {
             // create the new product
             let newProduct: ICartProduct = {
@@ -69,11 +74,25 @@ export const CartContextProvider = ({
                 quantity: 1
             }
 
-            setCart(( previousCart:ICart ) => [...previousCart, newProduct]);
+            setCart((previousCart: ICart) => [...previousCart, newProduct]);
         }
     }
 
-    const removeProductFromCart = (product: Steamer | Luggage | Accessory) => {
+    const increaseProductQuantity = (productAlreadyInCart: ICartProduct) => {
+        let indexOfProductInCart = cart.indexOf(productAlreadyInCart);
+        let newInstanceOfCart = [...cart];
+
+        let updatedProduct: ICartProduct = {
+            ...productAlreadyInCart,
+            quantity: productAlreadyInCart.quantity + 1
+        }
+
+        newInstanceOfCart[indexOfProductInCart] = updatedProduct;
+
+        return setCart((previousCart: ICart) => newInstanceOfCart);
+    }
+
+    const removeProductFromCart = (product: TProduct) => {
         let isProductAlreadyInCart = cart.find((cartProduct: ICartProduct) => cartProduct._id === product._id) || 0;
 
         if (isProductAlreadyInCart) {
@@ -91,12 +110,12 @@ export const CartContextProvider = ({
                 newInstanceOfCart[indexOfProductInCart] = updatedProduct;
             }
 
-            setCart(( previousCart:ICart ) => newInstanceOfCart);
+            setCart((previousCart: ICart) => newInstanceOfCart);
         }
     }
 
-    const isProductInCart = (product: Steamer | Luggage | Accessory) => {
-        let isProductAlreadyInCart = cart.find((cartProduct: ICartProduct) => cartProduct._id === product._id) || 0;
+    const isProductInCart = (productId: TProduct['_id'] | ICartProduct['_id']) => {
+        let isProductAlreadyInCart = cart.find((cartProduct: ICartProduct) => cartProduct._id === productId);
 
         if (isProductAlreadyInCart) {
             return true;
@@ -106,7 +125,7 @@ export const CartContextProvider = ({
     }
 
     return (
-        <CartContext.Provider value={{ cart, addProductToCart, removeProductFromCart, isProductInCart }}>
+        <CartContext.Provider value={{ cart, addProductToCart, removeProductFromCart, isProductInCart, increaseProductQuantity }}>
             {children}
         </CartContext.Provider>
     )
