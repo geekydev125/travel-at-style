21a8372cc3b12f88fd0af95acadcb97a83138280
geekydev125 <goldenstardev1108@gmail.com>
2024-04-22 09:00:14 +0300

    adding the missing classes attribute to the icon's svg

--- components/Icons/IconPlus.tsx
+++ components/Icons/IconPlus.tsx
@@ -13,7 +13,7 @@ function IconPlus({
     classes
 }: Props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={stroke}  style={{width: `${width}`, height: `${height}`}}>
+        <svg className={classes} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={stroke}  style={{width: `${width}`, height: `${height}`}}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
 
