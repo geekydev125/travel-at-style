a43b3315f8b7dfa63eaef80b621b52ba5a3e94ff
geekydev125 <goldenstardev1108@gmail.com>
2024-04-24 09:57:51 +0300

    Adding the displayNotification (with products names) to the drawer add/remove buttons

--- components/Cart/CardDrawerAddRemoveCartButtons.tsx
+++ components/Cart/CardDrawerAddRemoveCartButtons.tsx
@@ -1,4 +1,5 @@
 import { ICartProduct, useCartContext } from "@/context/cartContext"
+import { useNotificationContext } from "@/context/notificationContext"
 
 import IconPlus from "@/components/Icons/IconPlus"
 import IconMinus from "@/components/Icons/IconMinus"
@@ -11,15 +12,27 @@ function CardDrawerAddRemoveCartButtons({
     product,
 }: Props) {
     const { increaseProductQuantity, removeProductFromCart } = useCartContext()
+    const { displayNotification } = useNotificationContext()
+
+    function addProductHandler(product: ICartProduct) {
+        increaseProductQuantity(product._id)
+        displayNotification(`${product.name} ${product.color ? ` (${product.color})` : ''} added to cart`, 'success')
+    }
+
+    function removeProductHandler(product: ICartProduct) {
+        removeProductFromCart(product._id)
+        displayNotification(`${product.name} ${product.color ? ` (${product.color})` : ''} removed from cart`, 'warning')
+    }
+
     return (
         <>
-            <div style={{ cursor: 'pointer' }} onClick={() => removeProductFromCart(product._id)}>
+            <div style={{ cursor: 'pointer' }} onClick={() => removeProductHandler(product)}>
                 <IconMinus stroke="red" height="25px" width="25px" classes="shadow-sm" />
             </div>
 
             <p className="text-custom-dark mb-0 fw-semibold">${(product.price * product.quantity).toFixed(2)}</p>
 
-            <div style={{ cursor: 'pointer' }} onClick={() => increaseProductQuantity(product._id)}>
+            <div style={{ cursor: 'pointer' }} onClick={() => addProductHandler(product)}>
                 <IconPlus stroke="green" height="25px" width="25px" classes="shadow-sm" />
             </div>
         </>
