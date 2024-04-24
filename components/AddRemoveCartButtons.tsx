e48b46609a75d6ddd04e0bcec717aed13c97613e
geekydev125 <goldenstardev1108@gmail.com>
2024-04-24 09:30:11 +0300

    Implementing a displayNotification functionality to both buttons

--- components/AddRemoveCartButtons.tsx
+++ components/AddRemoveCartButtons.tsx
@@ -7,6 +7,7 @@ import { Steamer } from "@/model/Steamer"
 import CustomButton from "@/components/CustomButton"
 import IconPlus from "@/components/Icons/IconPlus"
 import IconMinus from "@/components/Icons/IconMinus"
+import { useNotificationContext } from "@/context/notificationContext"
 
 interface Props {
     product: Luggage | Accessory | Steamer,
@@ -20,17 +21,29 @@ function AddRemoveCartButtons({
     classesRemoveButton
 }: Props) {
     const { addProductToCart, removeProductFromCart, isProductInCart } = useCartContext()
+    const { displayNotification } = useNotificationContext()
+
+    function addProductHandler(product: Luggage | Accessory | Steamer) {
+        addProductToCart(product)
+        displayNotification('Product added to cart', 'success')
+    }
+
+    function removeProductHandler(productId: Luggage['_id'] | Accessory['_id'] | Steamer['_id']) {
+        removeProductFromCart(productId)
+        displayNotification('Product removed from cart', 'warning')
+    }
+
     return (
         <div className="d-flex flex-row">
             {
                 isProductInCart(product._id) && (
-                    <CustomButton classes={classesRemoveButton} variant="danger" size="sm" onClick={() => removeProductFromCart(product._id)}>
+                    <CustomButton classes={classesRemoveButton} variant="danger" size="sm" onClick={() => removeProductHandler(product._id)}>
                         <IconMinus />
                     </CustomButton>
                 )
             }
 
-            <CustomButton variant="success" onClick={() => addProductToCart(product)} size='sm' classes={classesAddButton}>
+            <CustomButton variant="success" onClick={() => addProductHandler(product)} size='sm' classes={classesAddButton}>
                 Add to cart&nbsp;
                 <IconPlus />
             </CustomButton>
