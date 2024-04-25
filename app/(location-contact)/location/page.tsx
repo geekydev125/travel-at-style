1a375e1ebb82c651edea4c0fe6db694406691d78
geekydev125 <goldenstardev1108@gmail.com>
2024-04-25 23:20:03 +0300

    Changing the title and restructuring the div with flexbox

--- app/(location-contact)/location/page.tsx
+++ app/(location-contact)/location/page.tsx
@@ -6,8 +6,8 @@ function LocationPage() {
 			<section className='container' style={{ height: '55vh' }}>
 				<div className='row h-100'>
 					<div className='col-10 offset-1 offset-sm-0 col-sm-7 col-md-5 col-lg-4 col-xl-3 d-flex flex-column justify-content-center align-items-center  shadow-lg' style={{ backgroundColor: 'rgba(0,0,0,0.8)' }} >
-						<div className='py-3 text-center'>
-							<h3 className="display-2 d-inline-block">Location</h3>
+						<div className='py-3 w-100 d-flex flex-column justify-content-center align-items-center'>
+							<h3 className="display-3 d-inline-block">Shop Location</h3>
 
 							<ul className="list-unstyled text-start">
 								<li>Fulton Center (lower level)</li>
