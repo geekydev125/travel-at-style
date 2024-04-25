43ed953a60fe3a554ab2153477bb67c1bb8816f9
geekydev125 <goldenstardev1108@gmail.com>
2024-04-25 23:21:03 +0300

    Changing the title's size

--- app/(location-contact)/contact/page.tsx
+++ app/(location-contact)/contact/page.tsx
@@ -6,7 +6,7 @@ function ContactPage() {
 			<section className="container p-3 p-md-5 rounded-4" style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}>
 				<div className="row">
 					<div className="col-12 col-md-5">
-						<h3 className="text-center display-5 ">Contact US</h3>
+						<h3 className="text-center display-3">Contact US</h3>
 						<p style={{textAlign: 'justify'}}>Our main company objective is to provide liable, continuous, and excellent customer service for our consumers. We look forward to your suggestions, comments and questions regarding our product.</p>
 						<p data-bs-theme="dark" className='text-muted'>*All form fields are required</p>
 
