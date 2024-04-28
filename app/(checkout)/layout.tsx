21ba1d417daab500a0c35b3a82a18ab6214ec1be
geekydev125 <goldenstardev1108@gmail.com>
2024-04-28 14:56:35 +0300

    Dynamically updating the titles depending on the pathname

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -12,11 +12,12 @@ function layout({
     children
 }: Props) {
     const pathname = usePathname()
+
     return (
         <Container as='section' className="py-lg-5">
             <div className='d-flex flex-row justify-content-between'>
                 <h3 className="display-2 text-custom-dark mb-4 fw-semibold">
-                    Checkout
+                    {pathname === '/checkout/review' ? 'Shopping Cart Review' : pathname === '/checkout/client-details' ? 'Client Details' : pathname === '/checkout/payment' ? 'Payment Details' : ''}
                 </h3>
 
                 {pathname === '/checkout' && (
