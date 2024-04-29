cd2741ca8cfe54c82545fefca95c29c8763f13d2
geekydev125 <goldenstardev1108@gmail.com>
2024-04-29 22:01:14 +0300

    Assigning the Props interface to the actual props

--- components/VideoOverlay.tsx
+++ components/VideoOverlay.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 function VideoOverlay({
 	alpha = 0.5
-}) {
+}:Props) {
 	return (
 		<div className="position-absolute top-0 left-0 w-100 h-100 z-1" style={{ backgroundColor: `rgba(0,0,0,${alpha})` }} />
 	)
