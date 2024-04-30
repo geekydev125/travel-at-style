0fd533b10dbe66a8dcc3f93c0f6ea9256b3006c3
geekydev125 <goldenstardev1108@gmail.com>
2024-04-30 22:18:27 +0300

    Creating the form layout with Rows/Cols

--- components/ClientDetailsViewComponents/ClientDetailsForm.tsx
+++ components/ClientDetailsViewComponents/ClientDetailsForm.tsx
@@ -11,6 +11,8 @@ import usStates from '@/data/usStates.json';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 interface FormData {
     firstName: string,
@@ -50,6 +52,8 @@ function ClientDetailsForm() {
     const onFormSubmit: SubmitHandler<FormData> = (formData: FormData, e) => {
         e?.preventDefault();
 
+        console.log(formData);
+
         // const response = fetch('/api/contact', {
         //     method: 'POST',
         //     headers: {
@@ -66,87 +70,128 @@ function ClientDetailsForm() {
 
     return (
         <Form onSubmit={handleSubmit(onFormSubmit)} data-bs-theme="dark" className='w-100'>
+            <Row className='gx-2'>
+                <Col md={6}>
+                    {/* First Name */}
+                    <FloatingLabel label="First Name" controlId="firstName" >
+                        <Form.Control isInvalid={errors.firstName && true} {...register('firstName')} type="text" name='firstName' />
 
-            <FloatingLabel label="First Name" controlId="firstName" >
-                <Form.Control isInvalid={errors.firstName && true} {...register('firstName')} type="text" name='firstName' />
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.firstName && errors.firstName.message}</p>
+                </Col>
 
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.firstName && errors.firstName.message}</p>
+                <Col md={6}>
+                    {/* Last Name */}
+                    <FloatingLabel label="Last Name" controlId="lastName" >
+                        <Form.Control isInvalid={errors.lastName && true} {...register('lastName')} type="text" name='lastName' />
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.lastName && errors.lastName.message}</p>
+                </Col>
+            </Row>
 
-            <FloatingLabel label="Last Name" controlId="lastName" >
-                <Form.Control isInvalid={errors.lastName && true} {...register('lastName')} type="text" name='lastName' />
-
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.firstName && errors.firstName.message}</p>
-
+            {/* Email Address */}
             <FloatingLabel label="Email Address" controlId="email" >
                 <Form.Control isInvalid={errors.email && true} {...register('email')} type="email" name='email' />
             </FloatingLabel>
             <p className="text-danger mb-2">{errors.email && errors.email.message}</p>
 
-            <FloatingLabel label="Country" controlId='country'>
-                <Form.Select {...register('country')} name='country' size='lg' defaultChecked={true} >
-                    {
-                        countries.map((country) => {
-                            return <option key={uniqid()} value={country.code}>{country.name}</option>
-                        })
-                    }
-                </Form.Select>
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.country && errors.country.message}</p>
+            <Row className='gx-2'>
+                <Col md={4}>
+                    {/* Country */}
+                    <FloatingLabel label="Country" controlId='country'>
+                        <Form.Select {...register('country')} name='country' size='lg' defaultChecked={true} >
+                            {
+                                countries.map((country) => {
+                                    return <option key={uniqid()} value={country.code}>{country.name}</option>
+                                })
+                            }
+                        </Form.Select>
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.country && errors.country.message}</p>
+                </Col>
 
-            <FloatingLabel label="Address" controlId="addressOne" >
-                <Form.Control isInvalid={errors.addressOne && true} {...register('addressOne')} type="text" name='addressOne' />
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.addressOne && errors.addressOne.message}</p>
+                <Col md={4}>
+                    {/* City */}
+                    <FloatingLabel label="City" controlId="city" >
+                        <Form.Control isInvalid={errors.city && true} {...register('city')} type="text" name='city' />
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.city && errors.city.message}</p>
+                </Col>
 
-            <FloatingLabel label="Apartment, suite, etc. (optional)" controlId="addressTwo" >
-                <Form.Control isInvalid={errors.addressTwo && true} {...register('addressTwo')} type="text" name='addressTwo' />
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.addressTwo && errors.addressTwo.message}</p>
+                <Col md={4}>
+                    {/* State */}
+                    {/* Make it conditional depending on the country */}
+                    <FloatingLabel label="State" controlId='state'>
+                        <Form.Select {...register('state')} name='state' size='lg' defaultChecked={true} >
+                            {
+                                usStates.map((state) => {
+                                    return <option key={uniqid()} value={state.code}>{state.name}</option>
+                                })
+                            }
+                        </Form.Select>
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.country && errors.country.message}</p>
+                </Col>
+            </Row>
 
-            <FloatingLabel label="City" controlId="city" >
-                <Form.Control isInvalid={errors.city && true} {...register('city')} type="text" name='city' />
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.city && errors.city.message}</p>
+            <Row className='gx-2'>
+                <Col md={5}>
+                    {/* Address 1 */}
+                    <FloatingLabel label="Address" controlId="addressOne" >
+                        <Form.Control isInvalid={errors.addressOne && true} {...register('addressOne')} type="text" name='addressOne' />
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.addressOne && errors.addressOne.message}</p>
+                </Col>
+                <Col md={5}>
 
-            {/* Make it conditional depending on the country */}
-            <FloatingLabel label="State" controlId='state'>
-                <Form.Select {...register('state')} name='state' size='lg' defaultChecked={true} >
-                    {
-                        usStates.map((state) => {
-                            return <option key={uniqid()} value={state.code}>{state.name}</option>
-                        })
-                    }
-                </Form.Select>
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.country && errors.country.message}</p>
+                    {/* Address 2 */}
+                    <FloatingLabel label="Apartment, suite, etc. (optional)" controlId="addressTwo" >
+                        <Form.Control isInvalid={errors.addressTwo && true} {...register('addressTwo')} type="text" name='addressTwo' />
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.addressTwo && errors.addressTwo.message}</p>
+                </Col>
 
-            <FloatingLabel label="ZIP code" controlId="zipCode" >
-                <Form.Control isInvalid={errors.zipCode && true} {...register('zipCode')} type="text" name='zipCode' />
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.zipCode && errors.zipCode.message}</p>
+                <Col md={2}>
+                    {/* ZIP Code */}
+                    <FloatingLabel label="ZIP code" controlId="zipCode" >
+                        <Form.Control isInvalid={errors.zipCode && true} {...register('zipCode')} type="text" name='zipCode' />
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.zipCode && errors.zipCode.message}</p>
+                </Col>
+            </Row>
 
-            {/* Make it controlled depending on the country */}
-            <FloatingLabel label="Country Code" controlId='countryCode'>
-                <Form.Select {...register('countryCode')} name='countryCode' size='lg' defaultChecked={true} >
-                    {
-                        countries.map((country) => {
-                            return <option key={uniqid()} value={country.code}>({country.name}) {country.dialCode}</option>
-                        })
-                    }
-                </Form.Select>
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.countryCode && errors.countryCode.message}</p>
+            <Row className='gx-2'>
+                <Col md={3}>
+                    {/* Country code */}
+                    {/* Make it controlled depending on the country */}
+                    <FloatingLabel label="Country Code" controlId='countryCode'>
+                        <Form.Select {...register('countryCode')} name='countryCode' size='lg' defaultChecked={true} >
+                            {
+                                countries.map((country) => {
+                                    return <option key={uniqid()} value={country.dialCode}>({country.name}) {country.dialCode}</option>
+                                })
+                            }
+                        </Form.Select>
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.countryCode && errors.countryCode.message}</p>
+                </Col>
+                <Col md={9}>
+                    {/* Phone Number */}
+                    <FloatingLabel label="Phone Number" controlId="phoneNumber" >
+                        <Form.Control isInvalid={errors.phoneNumber && true} {...register('phoneNumber')} type="text" name='phoneNumber' />
+                    </FloatingLabel>
+                    <p className="text-danger mb-2">{errors.phoneNumber && errors.phoneNumber.message}</p>
+                </Col>
+            </Row>
 
-            
-            <FloatingLabel label="Phone Number" controlId="phoneNumber" >
-                <Form.Control isInvalid={errors.phoneNumber && true} {...register('phoneNumber')} type="text" name='phoneNumber' />
-            </FloatingLabel>
-            <p className="text-danger mb-2">{errors.phoneNumber && errors.phoneNumber.message}</p>
 
+
+
+
+
+            {/* Notes */}
             <FloatingLabel label="Notes" controlId="notes" >
-                <Form.Control as='textarea' style={{height: '100px'}} isInvalid={errors.notes && true} {...register('notes')} type="text" name='notes' />
+                <Form.Control as='textarea' style={{ height: '100px' }} isInvalid={errors.notes && true} {...register('notes')} type="text" name='notes' />
             </FloatingLabel>
             <p className="text-danger mb-2">{errors.notes && errors.notes.message}</p>
 
