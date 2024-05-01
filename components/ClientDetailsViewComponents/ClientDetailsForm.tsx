9915d008ed56ba429d822de0ff2892a08025bb0a
geekydev125 <goldenstardev1108@gmail.com>
2024-05-01 13:56:00 +0300

    Creating dynamic country, state and country code inputs

--- components/ClientDetailsViewComponents/ClientDetailsForm.tsx
+++ components/ClientDetailsViewComponents/ClientDetailsForm.tsx
@@ -1,4 +1,6 @@
 "use client"
+import { useEffect, useState } from 'react';
+import { redirect } from 'next/navigation';
 import uniqid from 'uniqid';
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -8,12 +10,20 @@ import clientDetailsFormSchema from '@/validation/clientDetailsFormSchema';
 import countries from '@/data/countries.json';
 import usStates from '@/data/usStates.json';
 
+import CustomButton from '@/components/CustomButton';
+import ClientDetailsFormInput from '@/components/ClientDetailsViewComponents/ClientDetailsFormInput';
+import IconChevronRight from '@/components/Icons/IconChevronRight';
+
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import CustomButton from '../CustomButton';
-import ClientDetailsFormInput from './ClientDetailsFormInput';
+
+interface Country {
+    name: string,
+    code: string,
+    dialCode: string
+}
 
 export interface ClientDetailsFormData {
     firstName: string,
@@ -31,6 +41,10 @@ export interface ClientDetailsFormData {
 }
 
 function ClientDetailsForm() {
+    const [countryName, setCountryName] = useState<Country['name']>('United States');
+    const [usState, setUsState] = useState<string>('AL');
+    const [countryDialCode, setCountryDialCode] = useState<string>('');
+
     const { register, handleSubmit, formState: { errors, isDirty, isValid, isLoading } } = useForm<ClientDetailsFormData>({
         resolver: yupResolver(clientDetailsFormSchema),
         mode: 'onBlur',
@@ -38,22 +52,34 @@ function ClientDetailsForm() {
             firstName: '',
             lastName: '',
             email: '',
-            country: 'US',
+            country: countryName,
             addressOne: '',
             addressTwo: '',
             city: '',
-            state: 'AL',
+            state: usState,
             zipCode: '',
-            countryCode: '(United States) +1',
+            countryCode: countryDialCode,
             phoneNumber: '',
             notes: ''
         },
     });
 
+    useEffect(() => {
+        let country = countries.find((country) => country.name === countryName);
+        if (country) {
+            setCountryDialCode(`(${country.name}) ${country.dialCode}`);
+        }
+    }, [countryName])
+
     const onFormSubmit: SubmitHandler<ClientDetailsFormData> = (formData: ClientDetailsFormData, e) => {
         e?.preventDefault();
 
         console.log(formData);
+
+        let allGood = true;
+        if (allGood) {
+            redirect('/checkout/payment')
+        }
 
         // const response = fetch('/api/contact', {
         //     method: 'POST',
@@ -71,6 +97,7 @@ function ClientDetailsForm() {
 
     return (
         <Form onSubmit={handleSubmit(onFormSubmit)} data-bs-theme="dark" className='w-100'>
+            {/* First Name/Last Name */}
             <Row className='gx-2'>
                 <Col md={6}>
                     {/* First Name */}
@@ -86,42 +113,48 @@ function ClientDetailsForm() {
             {/* Email Address */}
             <ClientDetailsFormInput label='Email Address' type='email' register={register} name='email' errors={errors} />
 
+            {/* Country/City/State */}
             <Row className='gx-2'>
-                <Col md={4}>
+                <Col md={countryName === 'United States' ? 4 : 6}>
                     {/* Country */}
                     <FloatingLabel label="Country" controlId='country'>
-                        <Form.Select {...register('country')} name='country' size='lg' defaultChecked={true} >
+                        <Form.Select {...register('country', { onChange: (e) => setCountryName(e.target.value) })} name='country' size='lg' value={countryName}>
                             {
                                 countries.map((country) => {
-                                    return <option key={uniqid()} value={country.code}>{country.name}</option>
+                                    return <option key={uniqid()} value={country.name}>{country.name}</option>
                                 })
                             }
                         </Form.Select>
                     </FloatingLabel>
-                    <p className="text-danger mb-2">{errors.country && errors.country.message}</p>
+                    <p className="text-danger mb-2">{errors.country?.message}</p>
                 </Col>
 
-                <Col md={4}>
+                <Col md={countryName === 'United States' ? 4 : 6}>
                     {/* City */}
                     <ClientDetailsFormInput label='City' type='text' register={register} name='city' errors={errors} />
                 </Col>
 
-                <Col md={4}>
-                    {/* State */}
-                    {/* Make it conditional depending on the country */}
-                    <FloatingLabel label="State" controlId='state'>
-                        <Form.Select {...register('state')} name='state' size='lg' defaultChecked={true} >
-                            {
-                                usStates.map((state) => {
-                                    return <option key={uniqid()} value={state.code}>{state.name}</option>
-                                })
-                            }
-                        </Form.Select>
-                    </FloatingLabel>
-                    <p className="text-danger mb-2">{errors.country && errors.country.message}</p>
-                </Col>
+                {
+                    countryName === 'United States' && (
+                        <Col md={4}>
+                            {/* State */}
+                            {/* Make it conditional depending on the country */}
+                            <FloatingLabel label="State" controlId='state'>
+                                <Form.Select {...register('state', { onChange: (e) => setUsState(e.target.value) })} name='state' size='lg' value={usState}>
+                                    {
+                                        usStates.map((state) => {
+                                            return <option key={uniqid()} value={state.code}>{state.name}</option>
+                                        })
+                                    }
+                                </Form.Select>
+                            </FloatingLabel>
+                            <p className="text-danger mb-2">{errors.state?.message}</p>
+                        </Col>
+                    )
+                }
             </Row>
 
+            {/* Address 1/Address 2/ZIP code */}
             <Row className='gx-2'>
                 <Col md={5}>
                     {/* Address 1 */}
@@ -138,12 +171,13 @@ function ClientDetailsForm() {
                 </Col>
             </Row>
 
+            {/* Country code/Phone number */}
             <Row className='gx-2'>
                 <Col md={3}>
                     {/* Country code */}
                     {/* Make it controlled depending on the country */}
                     <FloatingLabel label="Country Code" controlId='countryCode'>
-                        <Form.Select {...register('countryCode')} name='countryCode' size='lg' defaultChecked={true}>
+                        <Form.Select {...register('countryCode', { onChange: (e) => setCountryDialCode(e.target.value) })} name='countryCode' size='lg' value={countryDialCode}>
                             {
                                 countries.map((country) => {
                                     return <option key={uniqid()} value={`(${country.name}) ${country.dialCode}`}>({country.name}) {country.dialCode}</option>
@@ -151,7 +185,7 @@ function ClientDetailsForm() {
                             }
                         </Form.Select>
                     </FloatingLabel>
-                    <p className="text-danger mb-2">{errors.countryCode && errors.countryCode.message}</p>
+                    <p className="text-danger mb-2">{errors.countryCode?.message}</p>
                 </Col>
                 <Col md={9}>
                     {/* Phone Number */}
@@ -163,11 +197,11 @@ function ClientDetailsForm() {
             <FloatingLabel label="Notes" controlId="notes" >
                 <Form.Control as='textarea' style={{ height: '100px' }} isInvalid={errors.notes && true} {...register('notes')} type="text" name='notes' />
             </FloatingLabel>
-            <p className="text-danger mb-2">{errors.notes && errors.notes.message}</p>
+            <p className="text-danger mb-2">{errors.notes?.message}</p>
 
             {/* Submit button */}
             <div className='d-flex justify-content-center'>
-                <CustomButton variant='primary' disabled={!(isDirty && isValid) || isLoading}>Submit</CustomButton>
+                <CustomButton variant='primary' disabled={!(isDirty && isValid) || isLoading} type="submit">Continue to Payment <IconChevronRight /></CustomButton>
             </div>
         </Form>
     )
