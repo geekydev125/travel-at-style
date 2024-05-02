ab3b34ab3ba4885874492da51bf8093f09800c0b
geekydev125 <goldenstardev1108@gmail.com>
2024-05-02 22:41:08 +0300

    Uppercasing the first call to action text

--- components/SteamersViewComponents/SectionComponents/FirstSectionComponents/FirstSectionCallToAction.tsx
+++ components/SteamersViewComponents/SectionComponents/FirstSectionComponents/FirstSectionCallToAction.tsx
@@ -1,12 +1,13 @@
 import NextLink from 'next/link'
 
 import styles from './FirstSectionCallToAction.module.scss'
+
 import CustomButton from '@/components/CustomButton'
 
 function FirstSectionCallToAction() {
     return (
         <section className={`${styles.section} text-center px-3 py-3 py-lg-4 background-gray-radial-gradient`} >
-            <h3 className="display-5 text-capitalize text-custom-light">The steamer that has taken America by storm now available for purchase online</h3>
+            <h3 className="display-5 text-uppercase text-custom-light">The steamer that has taken America by storm now available for purchase online</h3>
 
             <NextLink href='/shop'>
                 <CustomButton variant='primary' size='lg' classes='mt-1 mt-md-3'>
