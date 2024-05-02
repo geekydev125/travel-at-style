87a9803fc5833cbf17047b16f2c5895e76e88b7a
geekydev125 <goldenstardev1108@gmail.com>
2024-05-02 20:48:25 +0300

    Visually restructuring the product modal

--- components/ShopViewComponents/ProductModal.tsx
+++ components/ShopViewComponents/ProductModal.tsx
@@ -7,7 +7,6 @@ import { Luggage } from '@/model/Luggage';
 
 import AddRemoveCartButtons from '../AddRemoveCartButtons';
 
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
@@ -19,12 +18,14 @@ function ProductModal() {
     return (
         <>
             <Modal size='lg' centered show={showModal} onHide={() => showModalHandler(false, null)}>
-                {/* <Modal.Header closeButton={false} className='background-light-gradient text-custom-dark'>
+                <Modal.Header closeButton className='py-4 px-2 p-sm-4 background-light-gradient'>
                     <Modal.Title>
-                        {product && product.name}
-                        {`${(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}`}
+                        {/* <h4 className='text-custom-dark display-4 fw-semibold mb-0'>
+                            {product && product.name}
+                            {`${(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}`}
+                        </h4> */}
                     </Modal.Title>
-                </Modal.Header> */}
+                </Modal.Header>
 
                 <Modal.Body className='background-pattern-crossword text-custom-dark rounded-3'>
                     <Container>
@@ -40,14 +41,6 @@ function ProductModal() {
                                         {product && product.description}
                                     </p>
                                 </div>
-
-                                <p className='d-none d-lg-flex text-custom-dark display-5 fw-semibold mb-0'>
-                                    Price: {product && product.price}
-                                </p>
-
-                                <div className='d-none d-lg-flex flex-row justify-content-end align-items-center'>
-                                    <AddRemoveCartButtons classesRemoveButton='me-1' product={(product as Luggage | Accessory | Steamer)} />
-                                </div>
                             </Col>
                             <Col xs={12} lg={7} className='overflow-hidden '>
                                 {
@@ -59,24 +52,17 @@ function ProductModal() {
                                         />
                                     )
                                 }
-
-                                <div className='d-flex d-lg-none flex-row flex-sm-row justify-content-between align-items-center mt-4'>
-                                    <p className='text-custom-dark display-5 fw-semibold mb-0'>
-                                        Price: {product && product.price}
-                                    </p>
-
-                                    <AddRemoveCartButtons classesRemoveButton='me-1' product={(product as Luggage | Accessory | Steamer)} />
-                                </div>
                             </Col>
                         </Row>
                     </Container>
                 </Modal.Body>
 
-                <Modal.Footer className='background-light-gradient'>
-                    <Button variant="outline-primary" className='btn-sm' onClick={() => showModalHandler(false, null)}>
-                        Close
-                    </Button>
+                <Modal.Footer className='background-light-gradient py-4 px-2 p-sm-4 d-flex flex-row justify-content-between align-items-center'>
+                    <p className='text-custom-dark display-5 fw-semibold mb-0'>
+                        Price: {product && product.price}
+                    </p>
 
+                    <AddRemoveCartButtons classesRemoveButton='me-1' product={(product as Luggage | Accessory | Steamer)} />
                 </Modal.Footer>
             </Modal>
         </>
