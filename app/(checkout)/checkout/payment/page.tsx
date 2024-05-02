d50ef2f327be92952c4d9a43b9b33608b30d1297
geekydev125 <goldenstardev1108@gmail.com>
2024-05-02 18:28:39 +0300

    Creating the structure of the /checkout/payments page

--- app/(checkout)/checkout/payment/page.tsx
+++ app/(checkout)/checkout/payment/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Container from "react-bootstrap/Container"
 
 function PaymentPage() {
-  return (
-    <div>PaymentPage</div>
-  )
+	return (
+		<Container as="section" style={{height: '40vh'}} className="py-5 d-flex flex-column justify-content-center align-items-center">
+			
+			<p className="mt-5 display-4 text-danger fw-semibold">Travel @ Style is not accepting orders at this time. </p>
+			<p className="display-5 text-custom-dark">Please accept our apologies for the inconvenience.</p>
+
+			<div className="mt-4 d-flex flex-row align-items-center">
+				<p className="display-5 text-custom-dark">Sincerely,</p>
+				<img src="/assets/img/logo/logo-transparent.png" alt="Travel @ Style" className="img-fluid ms-lg-3" style={{height: '130px'}} />
+			</div>
+		</Container>
+	)
 }
 
 export default PaymentPage
