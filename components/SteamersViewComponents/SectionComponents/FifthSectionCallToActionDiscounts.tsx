61117f205103d2cb8541da50b8334270b86bffb2
geekydev125 <goldenstardev1108@gmail.com>
2024-05-04 18:39:12 +0300

    Fixing margins and text sizes in the call to action sections

--- components/SteamersViewComponents/SectionComponents/FifthSectionCallToActionDiscounts.tsx
+++ components/SteamersViewComponents/SectionComponents/FifthSectionCallToActionDiscounts.tsx
@@ -17,13 +17,13 @@ function FifthSectionCallToActionDiscounts() {
                     </Col>
 
                     <Col xs={{span: 12, order: 1}} md={{span: 6, order: 2}} lg={{span: 7, order: 2}}  className="text-center d-flex flex-column justify-content-center align-items-center">
-                        <h3 className="text-capitalize text-custom-light">Over the top quantity-based discounts</h3>
+                        <h3 className="text-uppercase text-custom-light mb-0">Over the top quantity-based discounts</h3>
 
-                        <NextLink href="/shop">
-                            <CustomButton variant="primary" size="lg" classes="mt-md-2 mb-3">GET YOUR STEAMER NOW!</CustomButton>
+                        <NextLink href="/shop" className="my-3 ">
+                            <CustomButton variant="primary" size="lg" classes="">GET YOUR STEAMER NOW!</CustomButton>
                         </NextLink>
 
-                        <h3 className="text-capitalize text-custom-light">Lifetime Warranty</h3>
+                        <h3 className="text-uppercase text-custom-light">Lifetime Warranty</h3>
                     </Col>
 
                     <Col xs={{span: 8, order: 3}} md={3} className="d-flex justify-content-center align-items-center ">
