77a4a5af4864da115ac5771bc250b234e8d220e6
geekydev125 <goldenstardev1108@gmail.com>
2024-05-05 18:56:03 +0300

    Restructuring and restyling the 404 (not-found) page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,28 +3,22 @@ import NextLink from 'next/link'
 import CustomButton from '@/components/CustomButton'
 
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+
+import IconChevronLeft from '@/components/Icons/IconChevronLeft'
 
 function NotFound() {
     return (
         <Container as="section" className='h-100 w-100 flex-grow-1 d-flex flex-column justify-content-center align-items-center py-5'>
-            <Row className='g-5'>
-
-                <Col xs={{order: 2}} md={{span: 2, offset: 1, order: 1}} className='d-flex align-items-center justify-content-center'>
-                    <img src="/assets/img/logo/logo-transparent.png" alt="Travel @ Style logo" className='img-fluid' style={{maxWidth: '120px'}} />
-                </Col>
-
-                <Col xs={{order: 1}} md={{span:9, order: 1}} className='d-flex flex-column justify-content-center align-items-center'>
-                    <h3 className='text-danger text-center display-3 fw-semibold'>Oops... 404 Error </h3>
-                    <p className='text-center display-6 fw-semibold'>(Page not found)</p>
-                    <NextLink href='/' >
-                        <CustomButton variant='primary' size='sm' >
-                            Click here to go back home
-                        </CustomButton>
-                    </NextLink>
-                </Col>
-            </Row>
+            <h3 className='text-danger text-center display-3'>Oops! Looks like you've wandered off course...</h3>
+            <p className='text-danger text-center display-6'>(404 Error - Page not found)</p>
+            <div className='mt-3'>
+                <NextLink href='/' >
+                    <CustomButton variant='primary'>
+                        <IconChevronLeft classes='me-1' />
+                        Click here to go back home
+                    </CustomButton>
+                </NextLink>
+            </div>
         </Container>
     )
 }
