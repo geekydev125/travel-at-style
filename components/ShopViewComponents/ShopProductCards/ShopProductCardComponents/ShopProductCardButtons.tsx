e55fe1744c570a9f54077de980180e78ee6b09b2
geekydev125 <goldenstardev1108@gmail.com>
2024-05-05 21:37:19 +0300

    Changing the product title button into a span and making the text bigger

--- components/ShopViewComponents/ShopProductCards/ShopProductCardComponents/ShopProductCardButtons.tsx
+++ components/ShopViewComponents/ShopProductCards/ShopProductCardComponents/ShopProductCardButtons.tsx
@@ -5,7 +5,6 @@ import Accessory from "@/model/Accessory"
 import Luggage from "@/model/Luggage"
 import Steamer from "@/model/Steamer"
 
-import Button from "react-bootstrap/Button"
 import AddRemoveCartButtons from "@/components/AddRemoveCartButtons"
 
 interface Props {
@@ -19,13 +18,13 @@ function ShopProductCardButtons({
 
     return (
         <div className="d-flex flex-row flex-sm-column flex-xl-row justify-content-between align-items-center px-2 py-4 position-relative overflow-hidden">
-            <Button variant='text' onClick={() => showModalHandler(true, product)} className="fw-bold h5 mb-0 px-md-0 px-lg-2">
+
+            <span onClick={() => showModalHandler(true, product)} className="fw-bold h5 mb-0 px-md-0 px-lg-2 display-6">
                 {product.name}
                 {(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}
-            </Button>
+            </span>
             
-            <AddRemoveCartButtons classesRemoveButton="me-1" product={product} />
-
+            <AddRemoveCartButtons classesAddButton="mt-sm-2 mt-xl-0" classesRemoveButton="me-1" product={product} />
         </div>
     )
 }
