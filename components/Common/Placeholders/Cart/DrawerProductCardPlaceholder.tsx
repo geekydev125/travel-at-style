6f0ffa25e742d3da201debf9123a7ab840dac87a
geekydev125 <goldenstardev1108@gmail.com>
2024-05-07 00:47:40 +0300

    Adding an icon image to the DrawerProductCardPlaceholder

--- components/Common/Placeholders/Cart/DrawerProductCardPlaceholder.tsx
+++ components/Common/Placeholders/Cart/DrawerProductCardPlaceholder.tsx
@@ -1,20 +1,28 @@
+import IconPhoto from '@/components/Icons/IconPhoto'
+
 import Placeholder from 'react-bootstrap/Placeholder'
 
 function DrawerProductCardPlaceholder() {
     return (
         <div className="border h-100 d-flex flex-column">
             <Placeholder as="div" className="d-flex flex-row" animation="glow" >
-                <Placeholder xs={12} style={{ minHeight: '124px' }} />
+                <Placeholder
+                    xs={12}
+                    className="d-flex flex-row justify-content-center align-items-center"
+                    style={{ minHeight: '124px' }}
+                >
+                    <IconPhoto stroke="black" width="50px" height="50px" />
+                </Placeholder>
             </Placeholder>
 
             <div className="bg-white">
                 <Placeholder animation="glow" className="w-100 d-flex flex-row flex-sm-column flex-xl-row justify-content-between align-items-center px-2 py-3">
 
-                    <Placeholder.Button variant="danger" size="sm" xs={1} className='p-0' style={{height: '25px', width: '25px'}} />
+                    <Placeholder.Button variant="danger" size="sm" xs={1} className='p-0' style={{ height: '25px', width: '25px' }} />
 
-                    <Placeholder xs={{span: 4}} />
+                    <Placeholder xs={{ span: 4 }} />
 
-                    <Placeholder.Button variant="success" size="sm" xs={1} className='p-0' style={{height: '25px', width: '25px'}} />
+                    <Placeholder.Button variant="success" size="sm" xs={1} className='p-0' style={{ height: '25px', width: '25px' }} />
                 </Placeholder>
             </div>
         </div>
