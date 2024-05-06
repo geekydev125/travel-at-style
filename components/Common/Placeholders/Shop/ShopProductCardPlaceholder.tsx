cdb36d8b3710fe4a4769fc08b38efba81b74cb13
geekydev125 <goldenstardev1108@gmail.com>
2024-05-07 00:45:31 +0300

    Adding an icon image to the ShopProductCardPlaceholder

--- components/Common/Placeholders/Shop/ShopProductCardPlaceholder.tsx
+++ components/Common/Placeholders/Shop/ShopProductCardPlaceholder.tsx
@@ -1,17 +1,26 @@
-import Placeholder from "react-bootstrap/Placeholder"
+import IconPhoto from "@/components/Icons/IconPhoto"
 
+import Placeholder from "react-bootstrap/Placeholder"
 
 function ShopProductCardPlaceholder() {
     return (
         <>
             <div className="w-100 border">
-                <Placeholder as="div" className="d-flex flex-row" animation="glow" >
-                    <Placeholder xs={12} style={{ minHeight: '285px' }} />
+                <Placeholder as="div" animation="glow" >
+                    <Placeholder
+                        xs={12}
+                        className="d-flex flex-row justify-content-center align-items-center"
+                        style={{ minHeight: '285px' }}
+                    >
+                        <IconPhoto stroke="black" width="50px" height="50px" />
+
+                    </Placeholder>
+
                 </Placeholder>
 
                 <div className="bg-white">
                     <Placeholder animation="glow" className="w-100 d-flex flex-row flex-sm-column flex-xl-row justify-content-between align-items-center px-2 py-4">
-                        <Placeholder xs={5} size="lg"/>
+                        <Placeholder xs={5} size="lg" />
 
                         <Placeholder.Button variant="success" size="sm" xs={4} className='mt-sm-2 mt-xl-0 py-1' />
                     </Placeholder>
