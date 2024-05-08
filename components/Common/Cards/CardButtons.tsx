c6621573d81e531d87adf5dcbcc0fad29827c4de
geekydev125 <goldenstardev1108@gmail.com>
2024-05-08 19:28:51 +0300

    Dynamically displaying product quantity on the shop cards

--- components/Common/Cards/CardButtons.tsx
+++ components/Common/Cards/CardButtons.tsx
@@ -1,8 +1,10 @@
 "use client"
+import { useEffect, useState } from "react"
 import NextLink from "next/link"
 
 import styles from '@/components/Common/Cards/Shop/ShopCard.module.scss'
 
+import { ICartProduct, useCartContext } from "@/context/cartContext"
 import { useModalContext } from "@/context/modalContext"
 
 import Accessory from "@/model/Accessory"
@@ -23,22 +25,40 @@ function CardButtons({
     cardType
 }: Props) {
     const { showModalHandler } = useModalContext()
-    const modalType = cardType
+    const { cart, isProductInCart } = useCartContext()
+    const modalType = cardType;
+    let [productQuantityInCart, setProductQuantityInCart] = useState<ICartProduct['quantity'] | 0>(0)
+
+    useEffect(() => {
+        let productInCart = isProductInCart(product._id)
+        if (productInCart) {
+            setProductQuantityInCart((productInCart as ICartProduct).quantity)
+        } else {
+            setProductQuantityInCart(0)
+        }
+    }, [cart])
 
     return (
-        <div className="d-flex flex-row flex-sm-column flex-xl-row justify-content-between align-items-center px-2 py-4 position-relative overflow-hidden">
+        <div className="d-flex flex-row flex-sm-column flex-xl-row justify-content-between align-items-start align-content-start px-2 pt-4 pb-1 position-relative overflow-hidden">
 
-            <span onClick={() => showModalHandler(true, product, modalType)} className={`${styles['shop-card-title']} mb-0 px-md-0 px-lg-2 display-6 fw-bold text-dark`}>
-                <span className='text-uppercase'>{product.name}</span>
-                {(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}
-            </span>
+            <div>
+                <span onClick={() => showModalHandler(true, product, modalType)} className={`${styles['shop-card-title']} mb-0 px-md-0 display-6 fw-bold text-dark`}>
+                    <span className='text-uppercase'>{product.name}</span>
+                    {(product as Steamer).color ? ` (${(product as Steamer).color})` : ''}
+                </span>
 
-            {cardType === 'shop' && <AddRemoveCartButtons classesAddButton="mt-sm-2 mt-xl-0" classesRemoveButton="me-1" product={product} />}
+                {productQuantityInCart && cardType === 'shop'
+                    ? <p className="mb-0">Quantity: {productQuantityInCart}</p>
+                    : <p style={{ minHeight: '8px' }}> </p>
+                }
+            </div>
+
+            {cardType === 'shop' && <AddRemoveCartButtons classesAddButton="mt-sm-2 mt-xl-0 text-nowrap" classesRemoveButton="me-1 text-nowrap" product={product} />}
             {cardType === 'product' && (
                 <NextLink href={`/shop?tab=${product.productCategory}`} >
-                    <CustomButton variant="primary" size="sm">
-                        GET YOURS NOW!
-                        <IconChevronRight />
+                    <CustomButton variant="primary" size="sm" classes="text-nowrap" >
+                        Get Yours Now!
+                        <IconChevronRight classes="ms-1" />
                     </CustomButton>
                 </NextLink>
             )}
