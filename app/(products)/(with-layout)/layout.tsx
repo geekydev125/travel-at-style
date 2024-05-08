6b0ad3cb0cdc24f35c39525de74dde9d6a206b6f
geekydev125 <goldenstardev1108@gmail.com>
2024-05-08 15:15:44 +0300

    Adding a dynamically loaded product modal to the products layout

--- app/(products)/(with-layout)/layout.tsx
+++ app/(products)/(with-layout)/layout.tsx
@@ -1,9 +1,11 @@
+import LuggageAccessoriesLayoutTitle from "@/components/Pages/LuggageAccessories/LuggageAccessoriesLayoutTitle";
+import FlyingAirplane from "@/components/Pages/LuggageAccessories/FlyingAirplane";
+
+import ProductModalDynamicLoader from "@/components/Common/ProductModal/ProductModalDynamicLoader";
+
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-import LuggageAccessoriesLayoutTitle from "@/components/Pages/LuggageAccessories/LuggageAccessoriesLayoutTitle";
-import FlyingAirplane from "@/components/Pages/LuggageAccessories/FlyingAirplane";
 
 interface Props {
     children: React.ReactNode
@@ -13,20 +15,24 @@ function ProductsLayout({
     children
 }: Props) {
     return (
-        <Container as='div' className="pt-4 pb-5 py-md-5">
-            <Row className="pb-4 pb-md-5">
-                <Col xs={4} md={5} className="d-flex flex-column justify-content-center align-items-center">
-                    <FlyingAirplane />
-                </Col>
-                <Col xs={8} md={7} className="d-flex flex-column justify-content-center">
-                    <LuggageAccessoriesLayoutTitle />
-                </Col>
-            </Row>
+        <>
+            <Container as='div' className="pt-4 pb-5 py-md-5">
+                <Row className="pb-4 pb-md-5">
+                    <Col xs={4} md={5} className="d-flex flex-column justify-content-center align-items-center">
+                        <FlyingAirplane />
+                    </Col>
+                    <Col xs={8} md={7} className="d-flex flex-column justify-content-center">
+                        <LuggageAccessoriesLayoutTitle />
+                    </Col>
+                </Row>
 
-            <Row as='section' className="gx-2 gx-md-3 gy-4 d-flex flex-row align-items-stretch flex-wrap">
-                {children}
-            </Row>
-        </Container>
+                <Row as='section' className="gx-2 gx-md-3 gy-4 d-flex flex-row align-items-stretch flex-wrap">
+                    {children}
+                </Row>
+            </Container>
+
+            <ProductModalDynamicLoader />
+        </>
     )
 }
 
