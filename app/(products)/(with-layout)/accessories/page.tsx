04454fad66d16aa8ae14eed99cfcdf87535b8a27
geekydev125 <goldenstardev1108@gmail.com>
2024-05-08 17:11:04 +0300

    Implementing the new ProductCard component into the luggage/accessories pages

--- app/(products)/(with-layout)/accessories/page.tsx
+++ app/(products)/(with-layout)/accessories/page.tsx
@@ -6,7 +6,7 @@ import accessories from "@/data/accessories.json"
 
 import Accessory from "@/model/Accessory";
 
-import LuggageAccessoriesProductCard from "@/components/Pages/LuggageAccessories/LuggageAccessoriesProductCard";
+import ProductCard from "@/components/Common/Cards/Product/ProductCard";
 
 import Col from "react-bootstrap/Col";
 
@@ -21,7 +21,7 @@ function AccessoriesPage() {
 				Object.values(accessories).map((accessory: Accessory) => {
 					return (
 						<Col xs={12} sm={6} md={4} className="d-flex flex-grow-1" key={uniqid()}>
-							<LuggageAccessoriesProductCard product={accessory} />
+							<ProductCard product={accessory} />
 						</Col>
 					)
 				})
--- app/(products)/(with-layout)/luggage/page.tsx
+++ app/(products)/(with-layout)/luggage/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next'
 import uniqid from 'uniqid'
 
-import LuggageAccessoriesProductCard from '@/components/Pages/LuggageAccessories/LuggageAccessoriesProductCard'
+import ProductCard from '@/components/Common/Cards/Product/ProductCard'
 
 import luggage from '@/data/luggage.json'
 
@@ -20,7 +20,7 @@ function LuggagePage() {
 				Object.values(luggage).map((Luggage: Luggage) => {
 					return (
 						<Col xs={12} sm={6} md={4} className="d-flex flex-grow-1" key={uniqid()}>
-							<LuggageAccessoriesProductCard product={Luggage} />
+							<ProductCard product={Luggage} />
 						</Col>
 					)
 				})
