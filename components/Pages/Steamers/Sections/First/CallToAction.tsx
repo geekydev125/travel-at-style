d53306a6f5cc2cdb50a68a0362244ab67e992fd2
geekydev125 <goldenstardev1108@gmail.com>
2024-05-09 11:43:55 +0300

    Adding the gray logo into the first call to action in the steamers page

--- components/Pages/Steamers/Sections/First/CallToAction.tsx
+++ components/Pages/Steamers/Sections/First/CallToAction.tsx
@@ -6,7 +6,7 @@ import CustomButton from '@/components/Common/Buttons/CustomButton'
 
 function CallToAction() {
     return (
-        <section className={`${styles.section} text-center px-3 py-3 py-lg-4 background-gray-radial-gradient`} >
+        <section className={`${styles.section} text-center px-3 py-3 py-lg-4 background-gray-radial-gradient position-relative`} >
             <h3 className="text-uppercase text-light">The steamer that has taken America by storm is now available for purchase online</h3>
 
             <NextLink href='/shop'>
@@ -14,6 +14,8 @@ function CallToAction() {
                     GET YOURS NOW!
                 </CustomButton>
             </NextLink>
+
+            <img src="/assets/img/logo/logo-gray.png" alt="logo" className={`z-0 ${styles['logo-img']}`}  />
         </section>
     )
 }
