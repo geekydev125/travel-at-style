cb8d567a21c30a5d01de9596507f4cc872b64210
geekydev125 <goldenstardev1108@gmail.com>
2024-05-10 14:21:23 +0300

    Creating the Luggage and Accessory mongoose Schemas

--- model/Accessory.ts
+++ model/Accessory.ts
@@ -1,4 +1,38 @@
+import { models, Schema, model } from "mongoose";
 import IProduct from "./Product";
 
-export interface IAccessory extends IProduct { 
+export interface IAccessory extends IProduct {
 }
+
+const AccessorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    productCategory: {
+        type: String,
+        required: true
+    },
+    nameCamelCase: {
+        type: String,
+        required: true
+    },
+    imgFolder: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+})
+
+export const Accessory = models.Accessory || model<IAccessory>('Accessory', AccessorySchema)
--- model/Luggage.ts
+++ model/Luggage.ts
@@ -1,4 +1,39 @@
+import { models, Schema, model } from "mongoose";
+
 import IProduct from "./Product";
 
 export interface ILuggage extends IProduct {
 }
+
+const LuggageSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    productCategory: {
+        type: String,
+        required: true
+    },
+    nameCamelCase: {
+        type: String,
+        required: true
+    },
+    imgFolder: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+})
+
+export const Luggage = models.Luggage || model<ILuggage>('Luggage', LuggageSchema)
