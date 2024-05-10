c2d0c54aaec2c10e76d02636790b90939969986b
geekydev125 <goldenstardev1108@gmail.com>
2024-05-10 15:14:44 +0300

    Implementing LuggageAccessoriesProducts into the luggage/accessories pages

--- app/(products)/(with-layout)/accessories/page.tsx
+++ app/(products)/(with-layout)/accessories/page.tsx
@@ -1,14 +1,6 @@
-import uniqid from "uniqid";
-
 import { Metadata } from "next";
 
-import accessories from "@/data/accessories.json"
-
-import { IAccessory } from "@/model/Accessory";
-
-import ProductCard from "@/components/Common/Cards/Product/ProductCard";
-
-import Col from "react-bootstrap/Col";
+import LuggageAccessoriesProducts from "@/components/Pages/LuggageAccessories/LuggageAccessoriesProducts";
 
 export const metadata: Metadata = {
     title: "Accessories",
@@ -16,17 +8,7 @@ export const metadata: Metadata = {
 
 function AccessoriesPage() {
 	return (
-		<>
-			{
-				accessories.map((accessory: IAccessory) => {
-					return (
-						<Col xs={12} sm={6} md={4} className="d-flex flex-grow-1" key={uniqid()}>
-							<ProductCard product={accessory} />
-						</Col>
-					)
-				})
-			}
-		</>
+		<LuggageAccessoriesProducts productCategory="accessories" />
 	)
 }
 
--- app/(products)/(with-layout)/luggage/page.tsx
+++ app/(products)/(with-layout)/luggage/page.tsx
@@ -1,30 +1,16 @@
 import { Metadata } from 'next'
-import uniqid from 'uniqid'
 
-import ProductCard from '@/components/Common/Cards/Product/ProductCard'
-
-import luggage from '@/data/luggage.json'
-
-import { ILuggage } from '@/model/Luggage'
-
-import Col from 'react-bootstrap/Col'
+import LuggageAccessoriesProducts from '@/components/Pages/LuggageAccessories/LuggageAccessoriesProducts'
 
 export const metadata: Metadata = {
     title: "Luggage",
 }
 
 function LuggagePage() {
+
 	return (
 		<>
-			{
-				luggage.map((product: ILuggage) => {
-					return (
-						<Col xs={12} sm={6} md={4} className="d-flex flex-grow-1" key={uniqid()}>
-							<ProductCard product={product} />
-						</Col>
-					)
-				})
-			}
+			<LuggageAccessoriesProducts productCategory="luggage" />
 		</ >
 	)
 }
