9127288b077b73d308661b809d7707160d81cf13
geekydev125 <goldenstardev1108@gmail.com>
2024-05-10 13:58:47 +0300

    Updating the Steamer model to also export a mongoose Schema

--- model/Steamer.ts
+++ model/Steamer.ts
@@ -1,4 +1,5 @@
 import IProduct from "./Product";
+import { Schema, model, models } from 'mongoose';
 
 export interface ISteamer extends IProduct {
     color: string,
@@ -7,3 +8,48 @@ export interface ISteamer extends IProduct {
 }
 
 
+const SteamerSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    productCategory: {
+        type: String,
+        required: true
+    },
+    nameCamelCase: {
+        type: String,
+        required: true
+    },
+    imgFolder: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    imgSpecial: {
+        type: String,
+        match: /^[a-zA-Z0-9_-]+\.(?:jpg|jpeg|png|gif|bmp)$/,
+        required: true
+    },
+    packageIncludes: {
+        type: [String],
+        required: true
+    }
+});
+
+export const Steamer = models.Steamer || model<ISteamer>('Steamer', SteamerSchema);
