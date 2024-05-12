6801612a0779065e2f5156dde540591c8b66383f
geekydev125 <goldenstardev1108@gmail.com>
2024-05-12 08:32:48 +0300

    Implementing the IconAnimatedScroll into the VideoOverlayContainer

--- components/Pages/Steamers/BesteamBackgroundVideo/VideoOverlay.tsx
+++ components/Pages/Steamers/BesteamBackgroundVideo/VideoOverlay.tsx
@@ -1,12 +1,16 @@
 interface Props {
 	alpha: number
+	children?: React.ReactNode
 }
 
 function VideoOverlay({
-	alpha = 0.5
-}:Props) {
+	alpha = 0.5,
+	children
+}: Props) {
 	return (
-		<div className="position-absolute top-0 left-0 w-100 h-100 z-1" style={{ backgroundColor: `rgba(0,0,0,${alpha})` }} />
+		<div className="position-absolute top-0 left-0 w-100 h-100 z-1" style={{ backgroundColor: `rgba(0,0,0,${alpha})` }}>
+			{children}
+		</div>
 	)
 }
 
--- components/Pages/Steamers/BesteamBackgroundVideo/VideoOverlayContainer.tsx
+++ components/Pages/Steamers/BesteamBackgroundVideo/VideoOverlayContainer.tsx
@@ -1,10 +1,14 @@
 import BackgroundVideo from "@/components/Pages/Steamers/BesteamBackgroundVideo/BesteamBackgroundVideo"
 import VideoOverlay from '@/components/Pages/Steamers/BesteamBackgroundVideo/VideoOverlay'
 
+import IconAnimatedScroll from '@/components/Icons/IconAnimatedScroll'
+
 function VideoOverlayContainer() {
 	return (
 		<>
-			<VideoOverlay alpha={0.4} />
+			<VideoOverlay alpha={0.4}>
+				<IconAnimatedScroll />
+			</VideoOverlay>
 			<BackgroundVideo />
 		</>
 	)
