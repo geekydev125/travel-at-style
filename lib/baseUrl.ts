2b004850a2f9d0b618cce9de9842808576791a41
geekydev125 <goldenstardev1108@gmail.com>
2024-05-14 16:32:02 +0300

    Creating a conditional baseUrl based on NODE_ENV

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,1 +1,3 @@
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = process.env.NODE_ENV === 'production'
+    ? 'https://travel-at-style.vercel.app'
+    : 'http://localhost:3000'
