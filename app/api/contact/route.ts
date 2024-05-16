7dc8fe6bb42c7e223674ed75721e10d2053df12c
geekydev125 <goldenstardev1108@gmail.com>
2024-05-16 13:17:37 +0300

    Implementing the validation of the contact POST request with the contactFormSchema

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,79 +1,78 @@
 import nodemailer from 'nodemailer'
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import contactFormSchema, { IContactFormData } from '@/validation/contactFormSchema';
+import { ObjectSchema } from 'yup';
 
 interface ContactFormData {
+    contactFormSchema: any;
     name: string,
     email: string,
     subject: string,
     message: string
 }
 
-export async function POST(req: Request, res: Response) {
-    // Data validation
-    // Error handling
+export async function POST(req: NextRequest, res: NextResponse) {
 
-    const { name, email, subject, message }: ContactFormData = await req.json();
-
-    const transporter = nodemailer.createTransport({
-        service: 'hotmail',
-        host: process.env.EMAIL_HOST,
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_FROM,
-            pass: process.env.EMAIL_FROM_PASSWORD,
-        },
-        from: process.env.EMAIL_FROM,
-        tls: {
-            rejectUnauthorized: false,
-        },
-    });
-
-    const output = `
-    <h2>You have a new contact form submission on Travel @ Style!</h2>
-    <h3>Submission Details</h3>
-    <ul>
-        <li><b>Name:</b> ${name}</li>
-        <li><b>Email address:</b> ${email}</li>
-        <li><b>Subject:</b> ${subject}</li>
-        <li><b>Message:</b> ${message}</li>
-    </ul>
-    `;
-
-    const options = {
-        from: `Travel @ Style <${process.env.EMAIL_FROM}>`,
-        to: process.env.EMAIL_TO,
-        subject: 'Travel @ Style contact form submission',
-        text: 'Travel @ Style contact form submission',
-        html: output,
-    }
-
+    const body: ContactFormData = await req.json();
     try {
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log("Server is ready to take our messages");
-            }
+        await (contactFormSchema as ObjectSchema<IContactFormData>).camelCase().validate(body, {
+            abortEarly: false,
+            strict: true
         });
 
-        const sendMail = await transporter.sendMail(options);
+        const { name, email, subject, message } = body;
 
-        // transporter.verify(function (err, success) {
-        //     if (err) {
-        //         console.log('err', err);
+        const transporter = nodemailer.createTransport({
+            service: 'hotmail',
+            host: process.env.EMAIL_HOST,
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_FROM,
+                pass: process.env.EMAIL_FROM_PASSWORD,
+            },
+            from: process.env.EMAIL_FROM,
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
 
-        //         return NextResponse.json({ message: 'An error occurred while attempting to process your message.' }, { status: 500 })
+        const output = `
+            <h2>You have a new contact form submission on Travel @ Style!</h2>
+            <h3>Submission Details</h3>
+            <ul>
+                <li><b>Name:</b> ${name}</li>
+                <li><b>Email address:</b> ${email}</li>
+                <li><b>Subject:</b> ${subject}</li>
+                <li><b>Message:</b> ${message}</li>
+            </ul>`;
 
-        //     } else {
-        //         console.log('Message sent: %s', sendMail.messageId);
-        //         return NextResponse.json({ success: success }, { status: 200 })
-        //     }
-        // });
 
-        return NextResponse.json({ message: 'Message sent successfully' });
-    } catch (error) {
-        console.log(error);
+        const options = {
+            from: `Travel @ Style <${process.env.EMAIL_FROM}>`,
+            to: process.env.EMAIL_TO,
+            subject: 'Travel @ Style contact form submission',
+            text: 'Travel @ Style contact form submission',
+            html: output,
+        }
+
+        transporter.verify(function (error, success) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Server is ready to take our messages");
+            }
+          });
+
+        const info = await transporter.sendMail(options);
+
+        if(info) {
+            return NextResponse.json({ status: 200, message: `Message sent successfully: %s ${info.messageId}` });
+        } else {
+            return NextResponse.json({ status: 500, message: 'An error occurred while attempting to process your message.' });
+        }
+    } catch (error: any) {
+        return NextResponse.json({ status: 400, error })
     }
-
 }
