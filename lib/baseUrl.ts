63a21ef2bb8a19c941017519d78220babf6b7151
geekydev125 <goldenstardev1108@gmail.com>
2024-05-20 11:55:45 +0300

    reversing the order of the baseUrl to check for "development" instead

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,3 +1,3 @@
-export const baseUrl = process.env.NODE_ENV === 'production'
-    ? 'https://travelatstyle.biz'
-    : 'http://localhost:3000'
+export const baseUrl = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://travelatstyle.biz'
