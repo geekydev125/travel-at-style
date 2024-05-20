5c147c2ad0bd0085dc7e5e7c06bc911b4408aee0
geekydev125 <goldenstardev1108@gmail.com>
2024-05-20 15:13:56 +0300

    Importing the products data directly from the shop page and passing it to the ShopProductTabs

--- app/(shop-checkout)/shop/page.tsx
+++ app/(shop-checkout)/shop/page.tsx
@@ -1,10 +1,20 @@
+import getProductsData from '@/lib/getProductsData';
+
+import { ISteamer } from "@/model/Steamer";
+import { ILuggage } from "@/model/Luggage";
+import { IAccessory } from "@/model/Accessory";
+
 import ShopProductsTabs from "@/components/Pages/Shop/ShopProductTabs"
 import ProductModalDynamicLoader from "@/components/Common/ProductModal/ProductModalDynamicLoader"
 
-function ShopPage() {
+async function ShopPage() {
+	const steamers: ISteamer[] = await getProductsData('steamers')
+	const luggage: ILuggage[] = await getProductsData('luggage')
+	const accessories: IAccessory[] = await getProductsData('accessories')
+
 	return (
 		<>
-			<ShopProductsTabs />
+			<ShopProductsTabs products={{steamers, luggage, accessories}} />
 
 			<ProductModalDynamicLoader />
 		</>
